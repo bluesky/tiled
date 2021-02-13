e8112b0f0b48a56ff118036f4dd1e4337b38a4c5
Dan Allan <dallan@bnl.gov>
2021-02-13 11:12:58 -0500

    Switch to file-based example.

--- catalog_server/example_catalogs.py
+++ catalog_server/example_catalogs.py
@@ -1,7 +1,22 @@
+import os
+from pathlib import Path
+
+import h5py
 import numpy
 
 from .datasources import ArraySource
 from .in_memory_catalog import Catalog
+
+
+def example_data(name, inner_name, value, size):
+    arr = value * numpy.ones((size, size))
+    path = Path("..", "example_data")
+    os.makedirs(path, exist_ok=True)
+    filename = f"{name}_{inner_name}.h5"
+    key = "data"
+    with h5py.File(path / filename, "w") as file:
+        file.create_dataset(key, data=arr)
+    return h5py.File(path / filename, "r")[key]
 
 
 # Build Catalog of Catalogs.
@@ -14,8 +29,8 @@ for name, size, fruit, animal in zip(
 ):
     subcatalogs[name] = Catalog(
         {
-            k: ArraySource(v * numpy.ones((size, size)))
-            for k, v in zip(["ones", "twos", "threes"], [1, 2, 3])
+            inner_name: ArraySource(example_data(name, inner_name, value, size))
+            for inner_name, value in zip(["ones", "twos", "threes"], [1, 2, 3])
         },
         metadata={"fruit": fruit, "animal": animal},
     )
