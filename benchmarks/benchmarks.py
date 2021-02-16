be012d65c43f6967c4c91fbd70b56098f7ab394a
gbischof <bisc8233@gmail.com>
2021-02-16 13:27:11 -0500

    minor update

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -21,24 +21,13 @@ class TimeSuite:
         self.server_process = subprocess.Popen((f"uvicorn catalog_server.server:app"
                                                 f" --host {HOST} --port {PORT}").split())
         time.sleep(5)
+        self.catalog = ClientCatalog.from_uri('http://' + HOST + ':' + PORT)
 
     def teardown(self):
         self.server_process.terminate()
 
-    def time_keys(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
-        for key in self.d.keys():
-            pass
-
-    def time_keys2(self):
-        self.d = {}
-        for x in range(500):
-            self.d[x] = None
-        for key in self.d.keys():
-            pass
-
     def time_list_catalog(self):
-        self.catalog = ClientCatalog.from_uri('http://' + HOST + ':' + PORT)
-        list(catalog)
+:       list(self.catalog)
+
+    def time_read(self):
+        self.catalog[-1].read()
