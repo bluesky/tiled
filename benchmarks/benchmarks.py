0bc6ad625965d7500736471000bc67b5dfa62ac9
Dan Allan <dallan@bnl.gov>
2021-02-27 14:09:34 -0500

    Address linting errors in benchmarks.

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -1,42 +1,44 @@
 # Write the benchmarking functions here.
 # See "Writing benchmarks" in the asv docs for more information.
-import asyncio
-import uvicorn
 import time
 import subprocess
 
 from catalog_server.client import ClientCatalog
-from catalog_server.server import app
-from catalog_server.example_catalogs import catalog
 
-HOST = '0.0.0.0'
+HOST = "0.0.0.0"
 PORT = 9040
+
 
 class TimeSuite:
     """
     An example benchmark that times the performance of various kinds
     of iterating over dictionaries in Python.
     """
+
     def setup(self):
-        self.server_process = subprocess.Popen((f"uvicorn catalog_server.server:app"
-                                                f" --host {HOST} --port {PORT}").split())
+        self.server_process = subprocess.Popen(
+            (
+                f"uvicorn catalog_server.server:app" f" --host {HOST} --port {PORT}"
+            ).split()
+        )
         time.sleep(5)
-        self.catalog = ClientCatalog.from_uri(f'http://{HOST}:{PORT}')
+        self.catalog = ClientCatalog.from_uri(f"http://{HOST}:{PORT}")
 
     def teardown(self):
         self.server_process.terminate()
+        self.server_process.wait()
 
     def time_list_catalog(self):
         list(self.catalog)
 
     def time_metadata(self):
-        self.catalog['medium']['ones'].metadata
+        self.catalog["medium"]["ones"].metadata
 
     def time_describe(self):
-        self.catalog['medium']['ones'].describe()
+        self.catalog["medium"]["ones"].describe()
 
     def time_read(self):
-        self.catalog['medium']['ones'].read()
+        self.catalog["medium"]["ones"].read()
 
     def time_compute(self):
-        self.catalog['medium']['ones'].read().compute()
+        self.catalog["medium"]["ones"].read().compute()
--- setup.py
+++ setup.py
@@ -3,13 +3,16 @@ import setuptools
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'requirements.txt')) as requirements_file:
+with open(path.join(here, "requirements.txt")) as requirements_file:
     # Parse requirements.txt, ignoring any commented-out lines.
-    requirements = [line for line in requirements_file.read().splitlines()
-                    if not line.startswith('#')]
+    requirements = [
+        line
+        for line in requirements_file.read().splitlines()
+        if not line.startswith("#")
+    ]
 
 setuptools.setup(
     name="catalog_server",
     install_requires=requirements,
-    packages=setuptools.find_packages(where='.', exclude=['doc', '.ci']),
+    packages=setuptools.find_packages(where=".", exclude=["doc", ".ci"]),
 )
