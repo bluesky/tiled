1d651cdb8ce91e037f31a75ad9faccfd49cb8073
Dan Allan <dallan@bnl.gov>
2021-03-07 16:36:03 -0500

    Add Server-timing header.

--- catalog_server/server/main.py
+++ catalog_server/server/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 
 from .settings import get_settings, get_custom_routers
 from .router import declare_search_route, router
@@ -25,6 +27,22 @@ async def shutdown_event():
     # client = get_dask_client()
     # await client.close()
     pass
+
+
+@api.middleware("http")
+async def add_server_timing_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    process_time = time.perf_counter() - start_time
+    # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Server-Timing
+    # https://w3c.github.io/server-timing/#the-server-timing-header-field
+    # Units are ms.
+    # This information seems safe to share because the user can easily
+    # estimate it based on request/response time, but if we add more detailed
+    # information here we should keep in mind security concerns and perhaps
+    # only include this for certain users.
+    response.headers["Server-Timing"] = f"app;dur={1000 * process_time:.1}"
+    return response
 
 
 if __name__ == "__main__":
