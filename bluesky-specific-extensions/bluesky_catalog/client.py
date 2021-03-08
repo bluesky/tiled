3c7b494f84972c8775e4742b2353fba72ee7caef
Dan Allan <dallan@bnl.gov>
2021-03-07 20:45:08 -0500

    Implement documents() on the client side.

--- bluesky-specific-extensions/bluesky_catalog/client.py
+++ bluesky-specific-extensions/bluesky_catalog/client.py
@@ -1,3 +1,5 @@
+import json
+
 from catalog_server.client.catalog import ClientCatalog
 
 
@@ -6,3 +8,11 @@ class BlueskyRun(ClientCatalog):
 
     def __repr__(self):
         return f"<{type(self).__name__}(uid={self.metadata['start']['uid']})>"
+
+    def documents(self):
+        # (name, doc) pairs are streamed as newline-delimited JSON
+        with self._client.stream(
+            "GET", f"/documents/{'/'.join(self._path)}"
+        ) as response:
+            for line in response.iter_lines():
+                yield tuple(json.loads(line))
