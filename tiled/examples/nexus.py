790ff5e03ccd64236f32b555e4e8507cc50585de
Dan Allan <dallan@bnl.gov>
2021-03-26 14:56:13 -0400

    Generalize Nexus example.

--- tiled/examples/nexus.py
+++ tiled/examples/nexus.py
@@ -1,7 +1,15 @@
+import io
 import os
 
 import h5py
+import httpx
 from tiled.catalogs.hdf5 import Catalog
 
 
-catalog = Catalog(h5py.File(os.environ["NEXUS_FILE_PATH"], "r"))
+# Download a Nexus file into a memory buffer.
+DEFAULT_EXAMPLE_URL = "https://github.com/nexusformat/exampledata/blob/master/APS/EPICSareaDetector/hdf5/AgBehenate_228.hdf5?raw=true"  # noqa
+buffer = io.BytesIO(httpx.get(os.getenv("NEXUS_URL", DEFAULT_EXAMPLE_URL)).content)
+# Access the buffer with h5py, which can treat it like a "file".
+file = h5py.File(buffer, "r")
+# Wrap the h5py.File in a Catalog to serve it with Tiled.
+catalog = Catalog(file)
