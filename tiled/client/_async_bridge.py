a4687ffb4b0132c8d7c8dea7045e5e36cf66084e
Dan Allan <dallan@bnl.gov>
2021-05-14 07:14:21 -0400

    Add comments.

--- tiled/client/_async_bridge.py
+++ tiled/client/_async_bridge.py
@@ -9,17 +9,37 @@ from .utils import Sentinel
 
 
 class AsyncClientBridge:
+    """
+    Run an httpx.AsyncClient on an event loop on its own thread.
+
+    EXPERIMENTAL: This may be removed or change in a backward-incompatible way
+    without notice.
+
+    This exposes just the parts of the AsyncClient API that we need.
+    Everything is exposed in a synchronous fashion.
+
+    Motivation: Wrapping an ASGI app only works with an async client, by its
+    construction. If we want a sync API, we need to bridge sync--async
+    somewhere.
+    """
     def __init__(self, **client_kwargs):
-        self._loop_starting = threading.Event()
+        # This will signal that the all the instance state that is defined in the
+        # # worker thread (e.g. self._client) has been defined.
+        # We will block on it before returning from __init__ to ensure that
+        # we never hand the caller a partially-formed object.
+        self._instance_state_setup_complete = threading.Event()
+        # This queue accepts requets like (callback, method_name, args, kwargs).
         self._queue = queue.Queue()
-        self._thread = threading.Thread(target=self._worker, args=(client_kwargs,))
+        self._thread = threading.Thread(
+            target=self._worker, args=(client_kwargs,), name="AsyncClient-worker")
         self._thread.start()
-        self._loop_starting.wait(timeout=1)
         # We could use any object here, but Sentinel is clearer for debugging.
         self._shutdown_sentinel = Sentinel("SHUTDOWN")
+        # TODO This finalizer is not working as intended.
         self._finalizer = weakref.finalize(
             self, self._queue.put, self._shutdown_sentinel
         )
+        self._instance_state_setup_complete.wait(timeout=1)
 
     def _worker(self, client_kwargs):
 
@@ -31,8 +51,9 @@ class AsyncClientBridge:
             # Note: This is important. The Tiled server routes are defined lazily on startup.
             await self._client._transport.app.router.startup()
 
-            self._loop_starting.set()
+            self._instance_state_setup_complete.set()
             while True:
+                # Poll the queue for work to do.
                 try:
                     item = self._queue.get_nowait()
                 except queue.Empty:
@@ -40,7 +61,7 @@ class AsyncClientBridge:
                     continue
                 if item is self._shutdown_sentinel:
                     break
-                method_name, args, kwargs, callback = item
+                callback, method_name, args, kwargs = item
                 future = asyncio.run_coroutine_threadsafe(
                     self._task(method_name, *args, **kwargs), loop
                 )
@@ -54,6 +75,7 @@ class AsyncClientBridge:
         return result
 
     def _run(self, method_name, *args, **kwargs):
+        "Submit to the worker. Return the result or raise the exception."
         response_queue = queue.Queue()
 
         def callback(future):
@@ -63,13 +85,14 @@ class AsyncClientBridge:
                 result = future.exception()
             response_queue.put(result)
 
-        self._queue.put((method_name, args, kwargs, callback))
+        self._queue.put((callback, method_name, args, kwargs))
         result = response_queue.get()
         if isinstance(result, Exception):
             raise result
         return result
 
     def shutdown(self, wait=True):
+        # TODO Why isn't this called at Python exit?
         print("shutdown")
         self._queue.put(self._shutdown_sentinel)
         if wait:
