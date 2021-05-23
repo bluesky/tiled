9050cb434fb03c570ffe9b109d507ba34e9e7829
Dan Allan <dallan@bnl.gov>
2021-05-23 12:15:40 -0400

    Install all optional deps

--- continuous_integration/scripts/install.sh
+++ continuous_integration/scripts/install.sh
@@ -8,5 +8,5 @@ python -m pip install --upgrade pip setuptools wheel numpy
 # Versioneer uses the most recent git tag to generate __version__, which appears
 # in the published documentation.
 git fetch --tags
-python -m pip install .
+python -m pip install '.[complete]'
 python -m pip list
