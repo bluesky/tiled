42ae2498c2bf820da0b244d7bacb106f5638a783
Dan Allan <dallan@bnl.gov>
2021-07-22 08:31:10 -0400

    Move constructors into a separate module from Node.

--- tiled/client/__init__.py
+++ tiled/client/__init__.py
@@ -1,10 +1,9 @@
-from .context import login  # noqa: F401
-from .node import (  # noqa: F401
-    ASCENDING,
-    DESCENDING,
+from .constructors import (  # noqa: F401
     from_tree,
     from_context,
     from_config,
     from_profile,
     from_uri,
 )
+from .context import login  # noqa: F401
+from .node import ASCENDING, DESCENDING  # noqa: F401
