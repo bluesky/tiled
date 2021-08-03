82fd751f806acd56515ab5f39ce16d15c87eab9b
jmaruland <jmaruland@bnl.gov>
2021-08-03 08:03:59 -0700

    TiffSequenceReader class
    Created a TiffSequenceReader class with read(), read_block(), microstructure() and macrostructure() methods.
    Added method to test read() and read_block()

---
+++ tiled/_tests/test_tiff.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import numpy
+import pandas
+import pytest
+import tifffile as tf
+
+from ..readers.array import ArrayAdapter
+from ..readers.tiff_sequence import TiffSequenceReader
+from ..readers.dataframe import DataFrameAdapter
+from ..client import from_tree
+from ..trees.in_memory import Tree
+
+
+@pytest.fixture
+def directory(tmpdir):
+    data = numpy.random.random((100, 100))
+    for i in range(10):
+        tf.imwrite(Path(tmpdir, f"temp{i:05}.tif"), data)
+
+    return str(Path(tmpdir, "*.tif"))
+
+@pytest.mark.parametrize('slice_input, correct_shape',
+                          [(None, (10,100,100)),
+                          (0, (100,100)),
+                          (slice(0, 10, 2), (5,100,100)),
+                          ((1, slice(0,10), slice(0,10)), (10,10)),
+                          ((slice(0,10), slice(0,10), slice(0,10)), (10,10,10)),
+                          ])
+def test_tiff_sequence(directory, slice_input, correct_shape):
+    tree = Tree(
+        {
+            "A": TiffSequenceReader(tf.TiffSequence(directory))
+            }
+        )
+    client = from_tree(tree)
+    arr=client["A"].read(slice=slice_input)
+    assert arr.shape == correct_shape
+    
+
+@pytest.mark.parametrize('block_input, correct_shape',
+                          [((0,0,0), (1,100,100))
+                          ])
+def test_tiff_sequence_block(directory, block_input, correct_shape):
+    tree = Tree(
+        {
+            "A": TiffSequenceReader(tf.TiffSequence(directory))
+            }
+        )
+    client = from_tree(tree)
+    arr=client["A"].read_block(block_input)
+    assert arr.shape == correct_shape
+
+
