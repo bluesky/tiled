931c984a783b934279fe02be7e37c8d603e1d905
Dan Allan <dallan@bnl.gov>
2021-09-20 09:11:51 -0400

    Test access control on sub-trees.

--- tiled/_tests/test_access_control.py
+++ tiled/_tests/test_access_control.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 import numpy
+import pytest
 
 from ..readers.array import ArrayAdapter
 from ..trees.in_memory import Tree
@@ -8,8 +9,14 @@ from ..client import from_config
 from ..server.authentication import create_refresh_token
 
 arr = ArrayAdapter.from_array(numpy.ones((5, 5)))
-tree_a = Tree({"A1": arr, "A2": arr})
-tree_b = Tree({"B1": arr, "B2": arr})
+
+
+def tree_a(access_policy):
+    return Tree({"A1": arr, "A2": arr}, access_policy=access_policy)
+
+
+def tree_b(access_policy):
+    return Tree({"B1": arr, "B2": arr}, access_policy=access_policy)
 
 
 def test_top_level_access_control():
@@ -28,6 +35,17 @@ def test_top_level_access_control():
             {
                 "tree": f"{__name__}:tree_a",
                 "path": "/a",
+                "access_control": {
+                    "access_policy": "tiled.trees.in_memory:SimpleAccessPolicy",
+                    "args": {
+                        "access_lists": {
+                            "alice": ["A2"],
+                            # This should have no effect because bob
+                            # cannot access the parent node.
+                            "bob": ["A1", "A2"],
+                        }
+                    },
+                },
             },
             {
                 "tree": f"{__name__}:tree_b",
@@ -36,18 +54,35 @@ def test_top_level_access_control():
         ],
     }
     # Directly generate a refresh token.
-    refresh_token = create_refresh_token(
+    alice_refresh_token = create_refresh_token(
         data={"sub": "alice"},
+        session_id=0,
+        session_creation_time=datetime.now(),
+        secret_key=SECRET_KEY,
+    )
+    bob_refresh_token = create_refresh_token(
+        data={"sub": "bob"},
         session_id=0,
         session_creation_time=datetime.now(),
         secret_key=SECRET_KEY,
     )
     # Provide the refresh token in a token cache. The client
     # will use this to "refresh" and obtain an access token.
-    client = from_config(
-        config, username="alice", token_cache={"refresh_token": refresh_token}
+    alice_client = from_config(
+        config, username="alice", token_cache={"refresh_token": alice_refresh_token}
     )
-    assert "a" in client
-    # Try accessing an item nested inside an item we can access.
-    client["a"]["A1"]
-    assert "b" not in client
+    bob_client = from_config(
+        config, username="bob", token_cache={"refresh_token": bob_refresh_token}
+    )
+    assert "a" in alice_client
+    assert "A2" in alice_client["a"]
+    assert "A1" not in alice_client["a"]
+    assert "b" not in alice_client
+    alice_client["a"]["A2"]
+    with pytest.raises(KeyError):
+        alice_client["b"]
+    assert not list(bob_client)
+    with pytest.raises(KeyError):
+        bob_client["a"]
+    with pytest.raises(KeyError):
+        bob_client["b"]
