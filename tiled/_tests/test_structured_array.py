e8320c27bd575c414b38abf63542fd6fa972d71c
Thomas A Caswell <tcaswell@bnl.gov>
2021-09-21 20:05:14 -0400

    TST: add structured array read test

--- tiled/_tests/test_structured_array.py
+++ tiled/_tests/test_structured_array.py
@@ -2,7 +2,10 @@ import numpy as np
 
 import pytest
 
+from tiled.client import from_tree
 from tiled.structures.structured_array import StructDtype
+from tiled.trees.in_memory import Tree
+from tiled.readers.array import StructuredArrayTabularAdapter
 
 
 @pytest.mark.parametrize(
@@ -23,3 +26,19 @@ def test_dtype_rount_trip(dtype, max_depth):
 def test_fail_subtype():
     with pytest.raises(ValueError):
         StructDtype.from_numpy_dtype(np.dtype("8f"))
+
+
+def test_read():
+    data = np.array(
+        [("Rex", 9, 81.0), ("Fido", 3, 27.0)],
+        dtype=[("name", "U10"), ("age", "i4"), ("weight", "f4")],
+    )
+    tree = Tree(
+        {
+            "A": StructuredArrayTabularAdapter.from_array(data),
+        },
+    )
+
+    client = from_tree(tree)
+
+    assert np.all(data == client["A"].read())
