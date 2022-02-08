17cda1a1ad0e5558d98a4895688c28f9b563b822
Ollie Copping <oliver.copping@diamond.ac.uk>
2022-02-08 09:37:34 +0000

    Parametrized test_json tests to make it neater.

--- tiled/_tests/test_json.py
+++ tiled/_tests/test_json.py
@@ -4,59 +4,32 @@ import pytest
 from ..utils import safe_json_dump
 
 
-@pytest.fixture
-def example_data():
-    example_data = {"test": numpy.array([0], dtype="i8")}
-    return example_data
+@pytest.mark.parametrize(
+    "test_params",
+    argvalues=[
+        {"array": numpy.array([0], dtype="i8")},
+        {"array": numpy.array([b"test"], dtype="|S4")},
+    ],
+)
+def test_ndarray(test_params):
+    safe_json_dump(test_params["array"])
 
 
-@pytest.fixture
-def example_bytes_data():
-    example_bytes_data = {"test": numpy.array([b"test"], dtype="|S4")}
-    return example_bytes_data
-
-
-@pytest.fixture
-def example_nested_data():
-    nested_data = {
+@pytest.mark.parametrize(
+    "test_params",
+    argvalues=[
+        {"array": numpy.array([0], dtype="i8")},
+        {"array": numpy.array([b"test"], dtype="|S4")},
+    ],
+)
+def test_nested_ndarray(test_params):
+    example_nested_data = {
         "data": [
             {
                 "id": "test",
-                "attributes": {"metadata": {"test": numpy.array([0], dtype="i8")}},
+                "attributes": {"metadata": {"test": test_params["array"]}},
             },
             {},
         ]
     }
-    return nested_data
-
-
-@pytest.fixture
-def example_nested_bytes_data():
-    nested_bytes_data = {
-        "data": [
-            {
-                "id": "test",
-                "attributes": {
-                    "metadata": {"test": numpy.array([b"test"], dtype="|S4")}
-                },
-            },
-            {},
-        ]
-    }
-    return nested_bytes_data
-
-
-def test_ndarray(example_data):
-    safe_json_dump(example_data)
-
-
-def test_bytes_ndarray(example_bytes_data):
-    safe_json_dump(example_bytes_data)
-
-
-def test_nested_ndarray(example_nested_data):
     safe_json_dump(example_nested_data)
-
-
-def test_nested_bytes_ndarray(example_nested_bytes_data):
-    safe_json_dump(example_nested_bytes_data)
