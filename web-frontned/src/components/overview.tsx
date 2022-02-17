cfdf5d6f1233a5db2b572a89c97a09199f883414
Dan Allan <dallan@bnl.gov>
2022-02-17 08:49:31 -0500

    Hoist state up.

--- web-frontned/src/components/overview.tsx
+++ web-frontned/src/components/overview.tsx
@@ -1,35 +1,21 @@
-import { useState, useEffect } from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { metadata } from '../client';
-import { components } from '../openapi_schemas';
 import JSONViewer from './json-viewer'
 
 interface IProps {
   segments: string[]
+  item: any
 }
 
 const NodeOverview: React.FunctionComponent<IProps> = (props) => {
-  const [item, setItem] = useState<components["schemas"]["Response_Resource_NodeAttributes__dict__dict___dict__dict_"]>();
-  // When props.segments updates, load metadata from that path.
-  useEffect(() => {
-    async function loadData() {
-      var result = await metadata(props.segments);
-      if (result !== undefined) {
-        console.log(result);
-        setItem(result);
-      }
-    }
-    loadData();
-  }, [props.segments]);
-  if (item && item.data) {
+  if (props.item && props.item.data) {
     return (
       <Box sx={{ my: 4 }}>
       <Container maxWidth="sm">
       <Typography variant="h4" component="h1" gutterBottom>
-        {item.data.id || "Top"}
-        <JSONViewer json={item} />
+        {props.item.data.id || "Top"}
+        <JSONViewer json={props.item} />
       </Typography>
       </Container>
       </Box>
--- web-frontned/src/routes/node.tsx
+++ web-frontned/src/routes/node.tsx
@@ -1,15 +1,29 @@
 import { useParams } from 'react-router-dom'
 import Contents from '../components/contents'
 import { NodeOverview } from '../components/overview'
+import { useState, useEffect } from 'react';
+import { metadata } from '../client';
+import { components } from '../openapi_schemas';
 
 function Node() {
   // Extract from path from react-router.
   const params = useParams<{"*": string}>();
   // Transform "/a/b/c" to ["a", "b", "c"].
   const segments = (params["*"] || "").split("/").filter(function (segment) {return segment})
+  const [item, setItem] = useState<components["schemas"]["Response_Resource_NodeAttributes__dict__dict___dict__dict_"]>();
+  useEffect(() => {
+    async function loadData() {
+      var result = await metadata(segments);
+      if (result !== undefined) {
+        console.log(result);
+        setItem(result);
+      }
+    }
+    loadData();
+  }, []);
   return (
     <div>
-      <NodeOverview segments={segments} />
+      <NodeOverview segments={segments} item={item} />
       <Contents segments={segments} />
     </div>
   )
