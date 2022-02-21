8aebe09eda8f6fe721a67594354e5de90a463c62
Dan Allan <dallan@bnl.gov>
2022-02-21 15:00:45 -0500

    Starting to play with DataGrid.

--- web-frontned/src/components/contents.tsx
+++ web-frontned/src/components/contents.tsx
@@ -4,10 +4,21 @@ import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
 import { search } from '../client';
 
+import * as React from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
+const columns = [
+  { field: 'id', headerName: 'ID', width: 200 },
+];
 
 interface IProps {
   segments: string[]
 }
+        // {items.map(id=> (
+        //  <li key={"li-" + id}>
+        //    <Link key={"link-" + id} to={"/node" + props.segments.map(function (segment) {return "/" + segment}) + "/" + id}>{id}</Link>
+        //  </li>
+        //))
 
 const Contents: React.FunctionComponent<IProps> = (props) => {
   const [items, setItems] = useState<string[]>([]);
@@ -21,20 +32,24 @@ const Contents: React.FunctionComponent<IProps> = (props) => {
     }
     loadData();
   }, [props.segments]);
-  return (
-    <Box sx={{ my: 4 }}>
-      <Container maxWidth="lg">
-        <ul>
-        {items.map(id=> (
-          <li key={"li-" + id}>
-            <Link key={"link-" + id} to={"/node" + props.segments.map(function (segment) {return "/" + segment}) + "/" + id}>{id}</Link>
-          </li>
-        ))
-        }
-        </ul>
-      </Container>
-    </Box>
-  );
+  if (items !== undefined) {
+    const rows = items.map((key) => ({id: key}));
+    return (
+      <Box sx={{ my: 4 }}>
+        <Container maxWidth="lg">
+          <div style={{ height: 400, width: '100%' }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              pageSize={10}
+            />
+          </div>
+        </Container>
+      </Box>
+    );
+  } else {
+    return <div>Loading...</div>
+  }
 }
 
 export default Contents;
