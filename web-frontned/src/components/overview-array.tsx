f0f050b82bf466694701ed15fa14f7f32487c5cf
Dan Allan <dallan@bnl.gov>
2022-02-24 08:11:49 -0500

    Factor RangeSlider into separate file.

--- web-frontned/src/components/overview-array.tsx
+++ web-frontned/src/components/overview-array.tsx
@@ -3,135 +3,16 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 
 import { ArrayLineChart } from "./line";
-import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import FormControl from "@mui/material/FormControl";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import FormLabel from "@mui/material/FormLabel";
-import Grid from "@mui/material/Grid";
-import Input from "@mui/material/Input";
 import Radio from "@mui/material/Radio";
 import RadioGroup from "@mui/material/RadioGroup";
+import RangeSlider from "./range-slider";
 import Skeleton from "@mui/material/Skeleton";
-import Slider from "@mui/material/Slider";
-import Typography from "@mui/material/Typography";
 import { axiosInstance } from "../client";
 import { debounce } from "ts-debounce";
-
-const LIMIT = 1000;
-
-interface RangeSliderProps {
-  min: number;
-  max: number;
-  setValue: any;
-  value: number[];
-}
-
-const RangeSlider: React.FunctionComponent<RangeSliderProps> = (props) => {
-  const handleSliderChange = (
-    event: Event,
-    newValue: number | number[],
-    activeThumb: number
-  ) => {
-    const range = newValue as number[];
-    let safeValue = [0, 0];
-    if (activeThumb === 0) {
-      safeValue = [range[0], Math.min(range[1], range[0] + LIMIT)];
-    } else {
-      safeValue = [Math.max(range[0], range[1] - LIMIT), range[1]];
-    }
-    props.setValue(safeValue as number[]);
-  };
-
-  const handleMinInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.setValue(
-      event.target.value === ""
-        ? props.value
-        : [
-            Number(event.target.value),
-            Math.min(props.value[1], Number(event.target.value) + LIMIT),
-          ]
-    );
-  };
-
-  const handleMaxInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    props.setValue(
-      event.target.value === ""
-        ? props.value
-        : [
-            Math.max(props.value[0], Number(event.target.value) - LIMIT),
-            Number(event.target.value),
-          ]
-    );
-  };
-
-  const handleBlur = () => {
-    if (props.value[0] < props.min) {
-      props.setValue([props.min, props.value[1]]);
-    } else if (props.value[1] > props.max) {
-      props.setValue([props.value[0], props.max]);
-    }
-  };
-
-  const marks = [
-    { value: props.min, label: props.min },
-    { value: props.max, label: props.max },
-  ];
-
-  return (
-    <Box sx={{ width: 500 }}>
-      <Typography id="input-slider" gutterBottom>
-        {props.max - props.min <= LIMIT
-          ? "Optionally slice a range of elements from the array"
-          : `Slice a range of up to ${LIMIT} elements of the array`}
-      </Typography>
-      <Grid container spacing={2} alignItems="center">
-        <Grid item xs>
-          <Slider
-            getAriaLabel={() => "Array slice range"}
-            value={props.value}
-            min={props.min}
-            max={props.max}
-            marks={marks}
-            onChange={handleSliderChange}
-            valueLabelDisplay="auto"
-          />
-        </Grid>
-        <Grid item>
-          <Input
-            value={props.value[0]}
-            size="small"
-            onChange={handleMinInputChange}
-            onBlur={handleBlur}
-            inputProps={{
-              step: 1,
-              min: props.min,
-              max: props.max,
-              type: "number",
-              "aria-labelledby": "min-input-slider",
-            }}
-          />
-        </Grid>
-        <Grid item>&ndash;</Grid>
-        <Grid item>
-          <Input
-            value={props.value[1]}
-            size="small"
-            onChange={handleMaxInputChange}
-            onBlur={handleBlur}
-            inputProps={{
-              step: 1,
-              min: props.min,
-              max: props.max,
-              type: "number",
-              "aria-labelledby": "max-input-slider",
-            }}
-          />
-        </Grid>
-      </Grid>
-    </Box>
-  );
-};
 
 interface DisplayRadioButtonsProps {
   value: string;
@@ -249,6 +130,8 @@ interface IProps {
   item: any;
 }
 
+const LIMIT = 1000;  // largest number of elements we will request and display at once
+
 const Array1D: React.FunctionComponent<IProps> = (props) => {
   const MAX_DEFAULT_RANGE = 100;
   const max = props.item.data.attributes.structure.macro.shape[0];
@@ -263,6 +146,7 @@ const Array1D: React.FunctionComponent<IProps> = (props) => {
         setValue={debounce(setValue, 100, { maxWait: 200 })}
         min={0}
         max={max}
+        limit={LIMIT}
       />
       <DataDisplay
         link={props.item.data.links.full}
