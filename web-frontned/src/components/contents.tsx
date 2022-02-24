1c092d4406312568414d06bf63e8c85f1d9b9b78
Dan Allan <dallan@bnl.gov>
2022-02-24 15:40:45 -0500

    Split NodeContents and Contents.

--- web-frontned/src/components/contents.tsx
+++ web-frontned/src/components/contents.tsx
@@ -1,48 +1,31 @@
 import * as React from "react";
 
 import { DataGrid, GridRowParams } from "@mui/x-data-grid";
-import { useEffect, useState } from "react";
 
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import { components } from "../openapi_schemas";
-import { search } from "../client";
 import { useNavigate } from "react-router-dom";
 
 const columns = [{ field: "id", headerName: "ID", width: 200 }];
 
 interface IProps {
-  segments: string[];
+  items: components["schemas"]["Resource_NodeAttributes__dict__dict_"][];
 }
 
 const DEFAULT_PAGE_SIZE = 10;
 
 const Contents: React.FunctionComponent<IProps> = (props) => {
-  const [pageSize, setPageSize] = React.useState<number>(DEFAULT_PAGE_SIZE);
-  const [items, setItems] = useState<
-    components["schemas"]["Resource_NodeAttributes__dict__dict_"][]
-  >([]);
   let navigate = useNavigate();
-  // When props.segments updates, load ids of children of that path.
-  useEffect(() => {
-    const controller = new AbortController();
-    async function loadData() {
-      var items = await search(props.segments, controller.signal);
-      setItems(items);
-    }
-    loadData();
-    return () => {
-      controller.abort();
-    };
-  }, [props.segments]);
-  const rows = items.map(
+  const [pageSize, setPageSize] = React.useState<number>(DEFAULT_PAGE_SIZE);
+  const rows = props.items.map(
     (item: components["schemas"]["Resource_NodeAttributes__dict__dict_"]) => ({
       id: item.id,
     })
   );
   type IdToAncestors = { [key: string]: string[] };
   var idsToAncestors: IdToAncestors = {};
-  items.map(
+  props.items.map(
     (item: components["schemas"]["Resource_NodeAttributes__dict__dict_"]) => {
       idsToAncestors[item.id as string] = item.attributes.ancestors;
     }
--- web-frontned/src/components/overview-generic-node.tsx
+++ web-frontned/src/components/overview-generic-node.tsx
@@ -1,6 +1,32 @@
+import { useEffect, useState } from "react";
+
 import Container from "@mui/material/Container";
 import Contents from "../components/contents";
 import Typography from "@mui/material/Typography";
+import { components } from "../openapi_schemas";
+import { search } from "../client";
+
+interface NodeContentsProps {
+  segments: string[];
+}
+
+const NodeContents: React.FunctionComponent<NodeContentsProps> = (props) => {
+  const [items, setItems] = useState<
+    components["schemas"]["Resource_NodeAttributes__dict__dict_"][]
+  >([]);
+  useEffect(() => {
+    const controller = new AbortController();
+    async function loadData() {
+      var items = await search(props.segments, controller.signal);
+      setItems(items);
+    }
+    loadData();
+    return () => {
+      controller.abort();
+    };
+  }, [props.segments]);
+  return <Contents items={items} />;
+};
 
 interface IProps {
   segments: string[];
@@ -13,7 +39,7 @@ const NodeOverview: React.FunctionComponent<IProps> = (props) => {
       <Typography id="table-title" variant="h6" component="h2">
         Contents
       </Typography>
-      <Contents segments={props.segments} />
+      <NodeContents segments={props.segments} />
     </Container>
   );
 };
