cce2634951eb2d60f8481ea9ed67c902858a1325
Dan Allan <dallan@bnl.gov>
2022-02-28 14:23:50 -0500

    Address warnings.

--- web-frontend/src/components/node-lazy-contents.tsx
+++ web-frontend/src/components/node-lazy-contents.tsx
@@ -20,19 +20,6 @@ interface Column {
   select_metadata: string;
 }
 
-interface Spec {
-  spec: string;
-  columns: Column[];
-  default_columns: string[];
-}
-
-interface IProps {
-  rowCount: number;
-  specs: string[];
-  columns: Column[];
-  defaultColumns: string[];
-}
-
 function CustomToolbar() {
   return (
     <GridToolbarContainer>
@@ -87,47 +74,46 @@ const NodeLazyContents: React.FunctionComponent<NodeLazyContentsProps> = (
   type IdsToAncestors = { [key: string]: string[] };
   const [idsToAncestors, setIdsToAncestors] = useState<IdsToAncestors>({});
   const [rowCount, setRowCount] = useState<number>(0);
-  async function loadItems(): Promise<
-    components["schemas"]["Resource_NodeAttributes__dict__dict_"][]
-  > {
-    var selectMetadata: string | null;
-    var fields: string[];
-    const controller = new AbortController();
-    if (props.columns.length === 0) {
-      // No configuration on which columns to show. Fetch only the ID.
-      fields = [];
-      selectMetadata = null;
-    } else {
-      fields = ["metadata"];
-      selectMetadata =
-        "{" +
-        props.columns
-          .map((column) => {
-            return `${column.field}:${column.select_metadata}`;
-          })
-          .join(",") +
-        "}";
-    }
-    var data = await search(
-      props.segments,
-      controller.signal,
-      fields,
-      selectMetadata,
-      rowsState.pageSize * rowsState.page,
-      rowsState.pageSize
-    );
-    setRowCount(data.meta!.count! as number);
-    const items = data.data;
-    return items!;
-  }
   useEffect(() => {
-    console.log("useEffect");
     let active = true;
+
+    async function loadItems(): Promise<
+      components["schemas"]["Resource_NodeAttributes__dict__dict_"][]
+    > {
+      var selectMetadata: string | null;
+      var fields: string[];
+      const controller = new AbortController();
+      if (props.columns.length === 0) {
+        // No configuration on which columns to show. Fetch only the ID.
+        fields = [];
+        selectMetadata = null;
+      } else {
+        fields = ["metadata"];
+        selectMetadata =
+          "{" +
+          props.columns
+            .map((column) => {
+              return `${column.field}:${column.select_metadata}`;
+            })
+            .join(",") +
+          "}";
+      }
+      var data = await search(
+        props.segments,
+        controller.signal,
+        fields,
+        selectMetadata,
+        rowsState.pageSize * rowsState.page,
+        rowsState.pageSize
+      );
+      setRowCount(data.meta!.count! as number);
+      const items = data.data;
+      return items!;
+    }
 
     (async () => {
       setRowsState((prev) => ({ ...prev, loading: true }));
       const newItems = await loadItems();
-      console.log("newItems", newItems);
       var idsToAncestors: IdsToAncestors = {};
       newItems.map(
         (
@@ -152,12 +138,10 @@ const NodeLazyContents: React.FunctionComponent<NodeLazyContentsProps> = (
       );
 
       if (!active) {
-        console.log("return early");
         return;
       }
 
       // TODO Synchronize these. (Clear rows first?)
-      console.log("set rows");
       setIdsToAncestors(idsToAncestors);
       setRowsState((prev) => ({ ...prev, loading: false, rows: newRows }));
     })();
@@ -165,7 +149,7 @@ const NodeLazyContents: React.FunctionComponent<NodeLazyContentsProps> = (
     return () => {
       active = false;
     };
-  }, [rowsState.page, rowsState.pageSize]);
+  }, [rowsState.page, rowsState.pageSize, props.columns, props.segments]);
 
   return (
     <Box sx={{ my: 4 }}>
