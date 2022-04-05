49dcef10bd256d4879bbb6ce0f9c107fb0bf82fa
Dan Allan <dallan@bnl.gov>
2022-04-05 10:55:01 -0400

    Actually run the indexing tests (!!!)

--- tiled/_tests/test_indexers.py
+++ tiled/_tests/test_indexers.py
@@ -6,18 +6,20 @@ from ..adapters.array import ArrayAdapter
 from ..adapters.mapping import MapAdapter
 from ..client import from_tree
 
+keys = list(string.ascii_lowercase)
 tree = MapAdapter(
     {
         letter: ArrayAdapter.from_array(number * numpy.ones(10))
-        for letter, number in zip(string.ascii_lowercase, range(26))
+        for letter, number in zip(keys, range(26))
     }
 )
 
 
 def test_indexers():
     client = from_tree(tree)
-    client.keys_indexer[:3] == list("abc")
-    client.keys_indexer[:1] == list("a")
-    client.keys_indexer[1:3] == list("bc")
-    client.keys_indexer[-3::-1] == list("zyx")
-    client.keys_indexer[:] == list(client) == string.ascii_lowercase
+    assert client.keys_indexer[:3] == keys[:3] == list("abc")
+    assert client.keys_indexer[:1] == keys[:1] == list("a")
+    assert client.keys_indexer[1:3] == keys[1:3] == list("bc")
+    assert client.keys_indexer[-3::-1] == keys[-3::-1]  # ["x", "w", "v", ...]
+    assert client.keys_indexer[-1:-5:-1] == keys[-1:-5:-1] == list("zyxw")
+    assert client.keys_indexer[:] == list(client) == keys
