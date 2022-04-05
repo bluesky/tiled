47e5a2b0c028d65e28dd6994d9d6ac8fbf74f569
Juan Marulanda <86245530+jmaruland@users.noreply.github.com>
2022-04-05 12:19:37 -0400

    Update .pre-commit-config.yaml (#196)
    * Update .pre-commit-config.yaml
    
    Update version of black in pre-commit-config.yml
    
    * reapply black

--- tiled/structures/image_serializer_helpers.py
+++ tiled/structures/image_serializer_helpers.py
@@ -98,7 +98,7 @@ def _scale(a, n, m, copy=True):
         Output image array. Has the same kind as `a`.
     """
     kind = a.dtype.kind
-    if n > m and a.max() < 2 ** m:
+    if n > m and a.max() < 2**m:
         mnew = int(np.ceil(m / 2) * 2)
         if mnew > m:
             dtype = "int{}".format(mnew)
@@ -126,11 +126,11 @@ def _scale(a, n, m, copy=True):
         # exact upscale to a multiple of `n` bits
         if copy:
             b = np.empty(a.shape, _dtype_bits(kind, m))
-            np.multiply(a, (2 ** m - 1) // (2 ** n - 1), out=b, dtype=b.dtype)
+            np.multiply(a, (2**m - 1) // (2**n - 1), out=b, dtype=b.dtype)
             return b
         else:
             a = a.astype(_dtype_bits(kind, m, a.dtype.itemsize), copy=False)
-            a *= (2 ** m - 1) // (2 ** n - 1)
+            a *= (2**m - 1) // (2**n - 1)
             return a
     else:
         # upscale to a multiple of `n` bits,
@@ -138,12 +138,12 @@ def _scale(a, n, m, copy=True):
         o = (m // n + 1) * n
         if copy:
             b = np.empty(a.shape, _dtype_bits(kind, o))
-            np.multiply(a, (2 ** o - 1) // (2 ** n - 1), out=b, dtype=b.dtype)
+            np.multiply(a, (2**o - 1) // (2**n - 1), out=b, dtype=b.dtype)
             b //= 2 ** (o - m)
             return b
         else:
             a = a.astype(_dtype_bits(kind, o, a.dtype.itemsize), copy=False)
-            a *= (2 ** o - 1) // (2 ** n - 1)
+            a *= (2**o - 1) // (2**n - 1)
             a //= 2 ** (o - m)
             return a
 
