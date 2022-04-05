5c17c4761df120436e3eaed5a0ce631ca19d3cca
Dan Allan <dallan@bnl.gov>
2022-04-05 10:55:25 -0400

    Provide a way to reverse-sort a MapAdapter.

--- tiled/adapters/mapping.py
+++ tiled/adapters/mapping.py
@@ -66,6 +66,11 @@ class MapAdapter(collections.abc.Mapping, IndexersMixin):
             Whether the client should strictly refresh stale cache items.
         """
         self._mapping = mapping
+        if sorting is None:
+            # This is a special case that means, "the given ordering".
+            # By giving that a name ("_") we enable requests to asking for the
+            # last N by requesting the sorting ("_", -1).
+            sorting = [("_", 1)]
         self._sorting = sorting
         self._metadata = metadata or {}
         if (access_policy is not None) and (
@@ -110,6 +115,10 @@ class MapAdapter(collections.abc.Mapping, IndexersMixin):
         # getting the wrong impression that editing this would update anything
         # persistent.
         return DictView(self._metadata)
+
+    @property
+    def sorting(self):
+        return list(self._sorting)
 
     def __repr__(self):
         return f"<{type(self).__name__}({set(self._mapping)!r})>"
@@ -191,12 +200,17 @@ class MapAdapter(collections.abc.Mapping, IndexersMixin):
     def sort(self, sorting):
         mapping = copy.copy(self._mapping)
         for key, direction in reversed(sorting):
-            mapping = dict(
-                sorted(
-                    mapping.items(),
-                    key=lambda item: item[1].metadata.get(key, _HIGH_SORTER),
+            if key == "_":
+                # Special case to enable reversing the given/default ordering.
+                # Leave mapping as is, and possibly reserve it below.
+                pass
+            else:
+                mapping = dict(
+                    sorted(
+                        mapping.items(),
+                        key=lambda item: item[1].metadata.get(key, _HIGH_SORTER),
+                    )
                 )
-            )
             if direction < 0:
                 if sys.version_info < (3, 8):
                     # Prior to Python 3.8 dicts are not reversible.
