f9c47ca3b0ceff24cb269e51bde5fe69706348ea
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-05-18 12:48:57 -0700

    add delete_content method to context

--- tiled/client/context.py
+++ tiled/client/context.py
@@ -558,6 +558,25 @@ Set an api_key as in:
             timestamp=3,  # Decode msgpack Timestamp as datetime.datetime object.
         )
 
+    def delete_content(self, path, content, headers=None):
+        # Submit CSRF token in both header and cookie.
+        # https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#double-submit-cookie
+        headers = headers or {}
+        headers.setdefault("x-csrf", self._client.cookies["tiled_csrf"])
+        headers.setdefault("accept", "application/x-msgpack")
+        request = self._client.build_request(
+            "DELETE",
+            path,
+            content=None,
+            headers=headers,
+        )
+        response = self._send(request)
+        handle_error(response)
+        return msgpack.unpackb(
+            response.content,
+            timestamp=3,  # Decode msgpack Timestamp as datetime.datetime object.
+        )
+
     def _send(self, request, stream=False, attempts=0):
         """
         If sending results in an authentication error, reauthenticate.
