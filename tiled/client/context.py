9bf5eb7dafc103ca19048d09cbcfe7bc02b49e60
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-05-18 12:47:21 -0700

    use context._send in put_content and post_metadata so that we revalidate if necessary

--- tiled/client/context.py
+++ tiled/client/context.py
@@ -532,14 +532,14 @@ Set an api_key as in:
                 "accept": "application/x-msgpack",
             },
         )
-        response = self._client.send(request)
+        response = self._send(request)
         handle_error(response)
         return msgpack.unpackb(
             response.content,
             timestamp=3,  # Decode msgpack Timestamp as datetime.datetime object.
         )
 
-    def put_content(self, path, content, headers=None, **kwargs):
+    def put_content(self, path, content, headers=None):
         # Submit CSRF token in both header and cookie.
         # https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#double-submit-cookie
         headers = headers or {}
@@ -551,7 +551,7 @@ Set an api_key as in:
             content=content,
             headers=headers,
         )
-        response = self._client.send(request)
+        response = self._send(request)
         handle_error(response)
         return msgpack.unpackb(
             response.content,
