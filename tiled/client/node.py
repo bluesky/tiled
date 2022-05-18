ae9950ea2eaa283a6315bb6be085bc57e88fee68
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-05-18 11:09:14 -0700

    invalidate length cache after calling write methods

--- tiled/client/node.py
+++ tiled/client/node.py
@@ -548,6 +548,11 @@ class Node(BaseClient, collections.abc.Mapping, IndexersMixin):
             # Do not print messy traceback from thread. Just fail silently.
             return []
 
+    def _invalidate_length_cache(self):
+        if self._cached_len is not None:
+            length, deadline = self._cached_len
+            self._cached_len = (length, time.monotonic())
+
     def write_array(self, array, metadata=None, specs=None):
         """
         EXPERIMENTAL: Write an array.
@@ -565,6 +570,8 @@ class Node(BaseClient, collections.abc.Mapping, IndexersMixin):
         """
 
         from ..structures.array import ArrayMacroStructure, ArrayStructure, BuiltinDtype
+
+        self._invalidate_length_cache()
 
         metadata = metadata or {}
         specs = specs or []
@@ -628,6 +635,8 @@ class Node(BaseClient, collections.abc.Mapping, IndexersMixin):
             DataFrameMicroStructure,
             DataFrameStructure,
         )
+
+        self._invalidate_length_cache()
 
         metadata = metadata or {}
         specs = specs or []
