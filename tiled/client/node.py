f6476df9b669a91d6045b780195b29367cfb5e19
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-06-10 11:57:47 -0700

    update client method

--- tiled/client/node.py
+++ tiled/client/node.py
@@ -363,7 +363,7 @@ class Node(BaseClient, collections.abc.Mapping, IndexersMixin):
         self._cached_len = None
 
         path = (
-            "/node/delete/"
+            "/node/metadata/"
             + "".join(f"/{part}" for part in self.context.path_parts)
             + "".join(f"/{part}" for part in self._path)
             + "/"
