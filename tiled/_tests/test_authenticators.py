f11efefde9731bc198c4fee9edb451541acb4ce8
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-09-01 22:59:52 -0400

    TST: unit test for LDAPAuthenticator

--- tiled/_tests/test_authenticators.py
+++ tiled/_tests/test_authenticators.py
@@ -5,8 +5,21 @@ import pytest
 from ..authenticators import LDAPAuthenticator
 
 
+# fmt: off
+@pytest.mark.parametrize("ldap_server_address, ldap_server_port", [
+    ("localhost", 1389),
+    ("localhost:1389", 904),  # Random port, ignored
+    ("localhost:1389", None),
+    ("127.0.0.1", 1389),
+    ("127.0.0.1:1389", 904),
+    (["localhost"], 1389),
+    (["localhost", "127.0.0.1"], 1389),
+    (["localhost", "127.0.0.1:1389"], 1389),
+    (["localhost:1389", "127.0.0.1:1389"], None),
+])
+# fmt: on
 @pytest.mark.parametrize("use_tls,use_ssl", [(False, False)])
-def test_LDAPAuthenticator_01(use_tls, use_ssl):
+def test_LDAPAuthenticator_01(use_tls, use_ssl, ldap_server_address, ldap_server_port):
     """
     Basic test for ``LDAPAuthenticator``.
 
@@ -14,8 +27,8 @@ def test_LDAPAuthenticator_01(use_tls, use_ssl):
     of the LDAP server.
     """
     authenticator = LDAPAuthenticator(
-        "localhost",
-        1389,
+        ldap_server_address,
+        ldap_server_port,
         bind_dn_template="cn={username},ou=users,dc=example,dc=org",
         use_tls=use_tls,
         use_ssl=use_ssl,
