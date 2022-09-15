8f02c066dda2aa128f99aea018234daefb4f018f
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-09-15 16:00:37 -0700

    improve docker (#322)
    * update Dockerfile so sparse deps are cached
    * don't copy whole source directory into container
    * make gunicorn config configurable using GUNICORN_CONF env var
    * require check that single_user_api_key is set even in public mode

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-/app/docker/check_config.py && exec gunicorn --config /app/docker/gunicorn_config.py
+DEFAULT_GUNICORN_CONF=/app/gunicorn_config.py
+export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
+
+/app/check_config.py && exec gunicorn --config $GUNICORN_CONF
