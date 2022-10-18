f004a6652e57214feaa927073d21eff02bd9c539
Dan Allan <dallan@bnl.gov>
2022-10-18 12:19:42 -0400

    Use public API.

--- tiled/_tests/test_client.py
+++ tiled/_tests/test_client.py
@@ -8,6 +8,6 @@ tree = MapAdapter({})
 
 def test_configurable_timeout():
     c = from_tree(tree)
-    assert c.context._client.timeout.connect != 17
+    assert c.context.http_client.timeout.connect != 17
     c = from_tree(tree, timeout=httpx.Timeout(17))
-    assert c.context._client.timeout.connect == 17
+    assert c.context.http_client.timeout.connect == 17
