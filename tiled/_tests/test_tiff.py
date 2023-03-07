71eeb55424bc698abff39d0bd001571f79e6686d
Dan Allan <dallan@bnl.gov>
2023-03-07 16:49:13 -0500

    More test refactor

--- tiled/_tests/test_tiff.py
+++ tiled/_tests/test_tiff.py
@@ -6,15 +6,35 @@ import tifffile as tf
 
 from ..adapters.mapping import MapAdapter
 from ..adapters.tiff import TiffAdapter, TiffSequenceAdapter, subdirectory_handler
-from ..client import from_config, from_tree
+from ..client import Context, from_context
+from ..server.app import build_app, build_app_from_config
+
+COLOR_SHAPE = (11, 17, 3)
 
 
-@pytest.fixture
-def directory(tmpdir):
+@pytest.fixture(scope="module")
+def client(tmpdir_module):
     data = numpy.random.random((5, 7))
+    sequence_directory = Path(tmpdir_module, "sequence")
+    sequence_directory.mkdir()
     for i in range(3):
-        tf.imwrite(Path(tmpdir, f"temp{i:05}.tif"), data)
-    return str(Path(tmpdir, "*.tif"))
+        tf.imwrite(sequence_directory / f"temp{i:05}.tif", data)
+    color_data = numpy.random.randint(0, 255, COLOR_SHAPE, dtype="uint8")
+    path = Path(tmpdir_module, "color.tif")
+    tf.imwrite(path, color_data)
+
+    tree = MapAdapter(
+        {
+            "color": TiffAdapter(str(path)),
+            "sequence": TiffSequenceAdapter(
+                tf.TiffSequence(str(sequence_directory / "*.tif"))
+            ),
+        }
+    )
+    app = build_app(tree)
+    with Context.from_app(app) as context:
+        client = from_context(context)
+        yield client
 
 
 @pytest.mark.parametrize(
@@ -27,18 +47,14 @@ def directory(tmpdir):
         ((slice(0, 3), slice(0, 3), slice(0, 3)), (3, 3, 3)),
     ],
 )
-def test_tiff_sequence(directory, slice_input, correct_shape):
-    tree = MapAdapter({"A": TiffSequenceAdapter(tf.TiffSequence(directory))})
-    client = from_tree(tree)
-    arr = client["A"].read(slice=slice_input)
+def test_tiff_sequence(client, slice_input, correct_shape):
+    arr = client["sequence"].read(slice=slice_input)
     assert arr.shape == correct_shape
 
 
 @pytest.mark.parametrize("block_input, correct_shape", [((0, 0, 0), (1, 5, 7))])
-def test_tiff_sequence_block(directory, block_input, correct_shape):
-    tree = MapAdapter({"A": TiffSequenceAdapter(tf.TiffSequence(directory))})
-    client = from_tree(tree)
-    arr = client["A"].read_block(block_input)
+def test_tiff_sequence_block(client, block_input, correct_shape):
+    arr = client["sequence"].read_block(block_input)
     assert arr.shape == correct_shape
 
 
@@ -78,23 +94,19 @@ a,b,c
             }
         ]
     }
-    client = from_config(config)
-    # This whole directory of files is one dataset.
-    assert client["sequence"].read().shape == (10, 100, 101)
-    # This directory has one dataset per file, in the normal fashion.
-    client["other_stuff"]["image"].read()
-    assert list(client["other_stuff"]["stuff"].read().columns) == ["a", "b", "c"]
-    assert list(client["other_file1"].read().columns) == ["a", "b", "c"]
-    assert list(client["other_file2"].read().columns) == ["a", "b", "c"]
+    app = build_app_from_config(config)
+    with Context.from_app(app) as context:
+        client = from_context(context)
+        # This whole directory of files is one dataset.
+        assert client["sequence"].read().shape == (10, 100, 101)
+        # This directory has one dataset per file, in the normal fashion.
+        client["other_stuff"]["image"].read()
+        assert list(client["other_stuff"]["stuff"].read().columns) == ["a", "b", "c"]
+        assert list(client["other_file1"].read().columns) == ["a", "b", "c"]
+        assert list(client["other_file2"].read().columns) == ["a", "b", "c"]
 
 
-def test_rgb(tmpdir):
+def test_rgb(client):
     "Test an RGB TIFF."
-    data = numpy.random.randint(0, 255, (11, 17, 3), dtype="uint8")
-    path = Path(tmpdir, "temp.tif")
-    tf.imwrite(path, data)
-
-    tree = MapAdapter({"A": TiffAdapter(str(path))})
-    client = from_tree(tree)
-    arr = client["A"].read()
-    assert arr.shape == data.shape
+    arr = client["color"].read()
+    assert arr.shape == COLOR_SHAPE
