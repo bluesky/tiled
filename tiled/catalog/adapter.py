5a913e3bee8f7c501fae91f306ae3f72f5df98a0
Dan Allan <dallan@bnl.gov>
2023-03-21 14:37:48 -0400

    WIP: Train arrived.

--- tiled/catalog/adapter.py
+++ tiled/catalog/adapter.py
@@ -14,36 +14,44 @@ async def initialize_database(engine):
 
 
 class Adapter:
-    def __init__(self, engine):
+    def __init__(self, engine, segments=None, queries=None):
         self.engine = engine
+        self.segments = segments or []
+        self.queries = queries or []
 
     @classmethod
     def from_uri(cls, database_uri):
-        # TODO Check that database exists and is initialized.
+        "Connect to an existing database."
+        # TODO Check that database exists and has the expected alembic revision.
         engine = create_async_engine(database_uri)
         return cls(engine)
 
     @classmethod
     def create_from_uri(cls, database_uri):
+        "Create a new database and connect to it."
         engine = create_async_engine(database_uri)
         asyncio.run(initialize_database(engine))
         return cls(engine)
 
     @classmethod
     def in_memory(cls):
+        "Create a transient database in memory."
         engine = create_async_engine("sqlite+aiosqlite:///:memory:")
         asyncio.run(initialize_database(engine))
         return cls(engine)
 
     def session(self, engine):
+        "Convenience method for constructing an AsyncSessoin context"
         return AsyncSession(self.engine, autoflush=False, expire_on_commit=False)
 
-    async def lookup(self, segments, principal):  # TODO: Accept filter for predicate-pushdown.
+    async def lookup(self, segments, principal=None):  # TODO: Accept filter for predicate-pushdown.
         async with self.session():
             ...
+            # Return something that can be searched or keys()/values()/items().
 
-    async def search(self, query):
-        ...
+    def search(self, query):
+        # Return something that can be searched or keys()/values()/items().
+        return type(self)(self.engine, self.queries + [query])
 
 
 class DatabaseNotFound(ValueError):
