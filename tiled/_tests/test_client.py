9f87998d0e856b37fca12710c2fb136f6ae2c730
Dan Allan <dallan@bnl.gov>
2023-03-27 16:03:37 -0400

    Test profile with 'direct' and fix bug.

--- tiled/_tests/test_client.py
+++ tiled/_tests/test_client.py
@@ -1,7 +1,11 @@
+from pathlib import Path
+
 import httpx
+import yaml
 
 from ..adapters.mapping import MapAdapter
-from ..client import Context, from_context
+from ..client import Context, from_context, from_profile
+from ..profiles import load_profiles, paths
 from ..server.app import build_app
 from .utils import fail_with_status_code
 
@@ -27,3 +31,24 @@ def test_client_version_check():
         context.http_client.headers["user-agent"] = "python-tiled/gibberish"
         with fail_with_status_code(400):
             list(client)
+
+
+def test_direct(tmpdir):
+    profile_content = {
+        "test": {
+            "direct": {
+                "trees": [
+                    {"path": "/", "tree": "tiled.examples.generated_minimal:tree"}
+                ]
+            }
+        }
+    }
+    with open(tmpdir / "example.yml", "w") as file:
+        file.write(yaml.dump(profile_content))
+    load_profiles.cache_clear()
+    profile_dir = Path(tmpdir)
+    try:
+        paths.insert(0, profile_dir)
+        from_profile("test")
+    finally:
+        paths.remove(profile_dir)
