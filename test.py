28dfaf18abd09d7a9d0f207f4dd2fe530fbb0ea2
Dan Allan <dallan@bnl.gov>
2023-03-31 08:38:23 -0400

    Debugging array writing

--- test.py
+++ test.py
@@ -1,50 +1,23 @@
 import asyncio
 
 from tiled._tests.test_catalog import temp_postgres
-from tiled.catalog.adapter import Adapter
-from tiled.catalog.explain import record_explanations
+from tiled.catalog.node import async_in_memory
+from tiled.client import Context, from_context
 from tiled.queries import Key
+from tiled.server.app import build_app
 
 
 async def test(a):
-    for i in range(100):
-        await a.create_node(
-            metadata={
-                "number": i,
-                "number_as_string": str(i),
-                "nested": {"number": i, "number_as_string": str(i)},
-            },
-            specs=[],
-            references=[],
-            structure_family="array",
-        )
-    # await a.create_metadata_index("nested_number_as_string", "nested.number_as_string")
-    # await a.create_metadata_index("nested_number", "nested.number")
-    with record_explanations() as e:
-        results = await a.search(Key("number_as_string") == "3").keys_slice(0, 5, 1)
-        assert len(results) == 1
-        results = await a.search(Key("number") == 3).keys_slice(0, 5, 1)
-        assert len(results) == 1
-        results = await a.search(Key("nested.number_as_string") == "3").keys_slice(
-            0, 5, 1
-        )
-        assert len(results) == 1
-        results = await a.search(Key("nested.number") == 3).keys_slice(0, 5, 1)
-        assert len(results) == 1
-    print(e)
+    app = build_app(a)
+    with Context.from_app(app) as context:
+        client = from_context(context)
+        print(list(client))
+        client.write_array([1, 2, 3])
 
 
 async def test_sqlite():
-    async with Adapter.async_in_memory(echo=True) as a:
+    async with async_in_memory(writable_storage="file:///tmp/data") as a:
         await test(a)
 
 
-async def test_postgres():
-    async with temp_postgres(
-        "postgresql+asyncpg://postgres:secret@localhost:5432", echo=True
-    ) as a:
-        await test(a)
-
-
-# asyncio.run(test_postgres())
 asyncio.run(test_sqlite())
