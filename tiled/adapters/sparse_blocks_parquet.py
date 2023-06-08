340115654b446b3095c50539e0e8af6502f2e691
Dan Allan <dallan@bnl.gov>
2023-06-08 17:28:00 -0400

    WIP

--- tiled/adapters/sparse_blocks_parquet.py
+++ tiled/adapters/sparse_blocks_parquet.py
@@ -1,41 +1,40 @@
 import itertools
 from pathlib import Path
 
-import dask.dataframe
+import pandas
+import sparse
 
+from ..adapters.array import slice_and_shape_from_block_and_chunks
 from ..structures.core import StructureFamily
-from .dataframe import DataFrameAdapter
 
 
-class ParquetDatasetAdapter:
+def load_block(uri):
+    # TODO This can be done without pandas.
+    # Better to use a plain I/O library.
+    df = pandas.read_parquet(uri)
+    coords = df[df.columns[:-1]].values.T
+    data = df["data"].values
+    return coords, data
+
+
+class SparseParquetBlocksAdapter:
     structure_family = StructureFamily.sparse
 
     def __init__(
         self,
-        *partition_paths,
+        *block_uris,
         meta,
         divisions,
         metadata=None,
         specs=None,
         references=None,
     ):
-        self.partition_paths = partition_paths
+        self.block_uris = block_uris
         self.meta = meta
         self.divisions = divisions
         self.metadata = metadata or {}
         self.specs = list(specs or [])
         self.references = list(references or [])
-
-    @property
-    def dataframe_adapter(self):
-        partitions = []
-        for path in self.partition_paths:
-            if not Path(path).exists():
-                partition = None
-            else:
-                partition = dask.dataframe.read_parquet(path)
-            partitions.append(partition)
-        return DataFrameAdapter(partitions, meta=self.meta, divisions=self.divisions)
 
     @classmethod
     def init_storage(cls, directory, chunks):
@@ -44,21 +43,24 @@ class ParquetDatasetAdapter:
         abs_directory = Path(directory).absolute()
         abs_directory.mkdir()
         num_blocks = (range(len(n)) for n in chunks)
-        assets = []
+        blocks = {}
         for block in itertools.product(*num_blocks):
             filepath = (
                 directory.absolute() / f"block-{'.'.join(map(str, block))}.parquet"
             )
-            assets.append(
-                Asset(
-                    data_uri=f"file://localhost{filepath}",
-                    is_directory=False,
-                )
+            uri = f"file://localhost{filepath}"
+            blocks[block] = uri
+        assets = [
+            Asset(
+                data_uri=uri,
+                is_directory=False,
             )
+            for uri in blocks.values()
+        ]
         return assets
 
     def write_block(self, data, block):
-        uri = self.partition_paths[partition]
+        uri = self.block_uris[block]
         data.to_parquet(uri)
 
     def write(self, data):
@@ -70,8 +72,14 @@ class ParquetDatasetAdapter:
     def read(self, *args, **kwargs):
         return self.dataframe_adapter.read(*args, **kwargs)
 
-    def read_partition(self, *args, **kwargs):
-        return self.dataframe_adapter.read_partition(*args, **kwargs)
+    def read_block(self, block, slice=...):
+        coords, data = load_block(self.block_uris[block])
+        _, shape = slice_and_shape_from_block_and_chunks(
+            block, self.doc.structure.chunks
+        )
+        arr = sparse.COO(data=data[:], coords=coords[:], shape=shape)
+        arr = arr[...]
+        return arr
 
     def macrostructure(self):
         return self.dataframe_adapter.macrostructure()
