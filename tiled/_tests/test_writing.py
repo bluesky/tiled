eeff33562183d353b2d46a9af8fe2d3ed0aafd6c
Dan Allan <dallan@bnl.gov>
2023-06-11 09:03:57 -0400

    Test revisions.

--- tiled/_tests/test_writing.py
+++ tiled/_tests/test_writing.py
@@ -322,3 +322,22 @@ def test_limits(tree):
             )
         with fail_with_status_code(422):
             y.update_metadata(references=too_many_chars)
+
+
+def test_metadata_revisions(tree):
+    with Context.from_app(build_app(tree)) as context:
+        client = from_context(context)
+        ac = client.write_array([1, 2, 3], key="revise_me")
+        assert len(ac.metadata_revisions[:]) == 0
+        ac.update_metadata(metadata={"a": 1})
+        assert ac.metadata["a"] == 1
+        client["revise_me"].metadata["a"] == 1
+        assert len(ac.metadata_revisions[:]) == 1
+        ac.update_metadata(metadata={"a": 2})
+        assert ac.metadata["a"] == 2
+        client["revise_me"].metadata["a"] == 2
+        assert len(ac.metadata_revisions[:]) == 2
+        ac.metadata_revisions.delete_revision(1)
+        assert len(ac.metadata_revisions[:]) == 1
+        with fail_with_status_code(404):
+            ac.metadata_revisions.delete_revision(1)
