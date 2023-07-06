56b8a61b7a240886ae764008fa101f9076ac6d3f
Dan Allan <dallan@bnl.gov>
2023-07-06 12:32:48 -0400

    WIP

--- tiled/client/cache/database.py
+++ tiled/client/cache/database.py
@@ -4,21 +4,30 @@ from pathlib import Path
 
 from .. import context
 
-
 VERSION = 1
 
 
 def default_filepath(api_uri):
     # TO DO: If TILED_CACHE_DIR is on NFS, use a sqlite temporary database instead.
     return Path(
-        context.TILED_CACHE_DIR, "http_response_cache", urllib.parse.quote_plus(str(api_uri))
+        context.TILED_CACHE_DIR,
+        "http_response_cache",
+        urllib.parse.quote_plus(str(api_uri)),
     )
 
 
 class Cache:
-    def __init__(self, filepath, total_capacity=500_000_000, max_item_size=500_000, readonly=False):
+    def __init__(
+        self,
+        filepath,
+        total_capacity=500_000_000,
+        max_item_size=500_000,
+        readonly=False,
+    ):
         if readonly:
-            raise NotImplementedError("readonly cache is planned but not yet implemented")
+            raise NotImplementedError(
+                "readonly cache is planned but not yet implemented"
+            )
         self.total_capacity = total_capacity
         self.max_item_size = max_item_size
         self._readonly = readonly
@@ -32,10 +41,14 @@ class Cache:
         elif "tiled_http_response_cache_version" not in tables:
             # We have a nonempty database that we do not recognize.
             print(tables)
-            raise RuntimeError(f"Database at {filepath} is not empty and not recognized as a tiled HTTP response cache.")
+            raise RuntimeError(
+                f"Database at {filepath} is not empty and not recognized as a tiled HTTP response cache."
+            )
         else:
             # We have a nonempty database that we recognize.
-            cursor = self._db.execute("SELECT * FROM tiled_http_response_cache_version;")
+            cursor = self._db.execute(
+                "SELECT * FROM tiled_http_response_cache_version;"
+            )
             (version,) = cursor.fetchone()
             if version != VERSION:
                 # It is likely that this cache database will be very stable; we
@@ -46,19 +59,25 @@ class Cache:
                 self._db = sqlite3.connect(filepath)
                 self._create_tables()
 
-
     def _create_tables(self):
         with closing(self._db.cursor()) as cur:
-            cur.execute("""CREATE TABLE responses (
+            cur.execute(
+                """CREATE TABLE responses (
 cache_key TEXT,
 headers JSON,
 body BLOB,
 size INTEGER,
 time_created TEXT,
 time_last_accessed TEXT
-)""")
-            cur.execute("CREATE TABLE tiled_http_response_cache_version (version INTEGER)")
-            cur.execute("INSERT INTO tiled_http_response_cache_version (version) VALUES (?)", (VERSION,))
+)"""
+            )
+            cur.execute(
+                "CREATE TABLE tiled_http_response_cache_version (version INTEGER)"
+            )
+            cur.execute(
+                "INSERT INTO tiled_http_response_cache_version (version) VALUES (?)",
+                (VERSION,),
+            )
             self._db.commit()
 
     @property
