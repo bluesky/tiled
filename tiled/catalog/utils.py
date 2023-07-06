28e8b1dd173a4bc311770b86cdf6b5cf13a85eb1
Dan Allan <dallan@bnl.gov>
2023-07-06 16:11:31 -0400

    Add docstrings, comments, and fix POSIX issue.

--- tiled/catalog/utils.py
+++ tiled/catalog/utils.py
@@ -1,6 +1,7 @@
 import re
 import sys
 from pathlib import Path
+from urllib import parse
 
 import httpx
 
@@ -8,6 +9,13 @@ SCHEME_PATTERN = re.compile(r"^[a-z0-9]+:\/\/.*$")
 
 
 def safe_path(uri):
+    """
+    Acceess the path of a URI and return it as a Path object.
+
+    Ideally we could just do uri.path, but Windows paths confuse
+    HTTP URI parsers because of the drive (e.g. C:) and return
+    something like /C:/x/y/z with an extraneous leading slash.
+    """
     raw_path = httpx.URL(uri).path
     if sys.platform == "win32" and raw_path[0] == "/":
         path = raw_path[1:]
@@ -16,10 +24,18 @@ def safe_path(uri):
     return Path(path)
 
 
-def ensure_uri(uri):
-    if not SCHEME_PATTERN.match(uri):
-        uri = "file://localhost/" + str(Path(uri).absolute())
-    uri = httpx.URL(uri)
+def ensure_uri(uri_or_path):
+    "Accept a URI or file path (Windows- or POSIX-style) and return a URI."
+    if not SCHEME_PATTERN.match(uri_or_path):
+        # Interpret this as a filepath.
+        path = uri_or_path
+        uri_str = parse.urlunparse(("file", "localhost", str(path), "", "", None))
+    else:
+        # Innterpret this as a URI.
+        uri_str = uri_or_path
+    uri = httpx.URL(uri_str)
+    # Ensure that, if the scheme is file, it meets the techincal standard for
+    # file URIs, like file://localhost/..., not the shorthand file:///...
     if uri.scheme == "file":
         uri = uri.copy_with(host="localhost")
     return uri
