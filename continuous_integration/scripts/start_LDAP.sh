0483ad9fb92bb31da65a3e8e3a7a5994dd2b1bcc
Dan Allan <dallan@bnl.gov>
2023-07-09 11:28:17 -0400

    Add PostgreSQL to CI.

---
+++ continuous_integration/scripts/start_LDAP.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+# Start LDAP server in docker container
+docker pull bitnami/openldap:latest
+docker-compose -f continuous_integration/docker-configs/ldap-docker-compose.yml up -d
+docker ps
