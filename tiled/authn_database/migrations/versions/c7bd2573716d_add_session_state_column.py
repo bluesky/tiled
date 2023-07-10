396869cd5dddb8d2aeb3862e16523cf14136fb70
Dylan McReynolds <dmcreynolds@lbl.gov>
2023-07-10 14:03:53 -0700

    add alembic migration for Session state

---
+++ tiled/authn_database/migrations/versions/c7bd2573716d_add_session_state_column.py
@@ -0,0 +1,27 @@
+"""add session state column
+
+Revision ID: c7bd2573716d
+Revises: 4a9dfaba4a98
+Create Date: 2023-07-10 13:57:12.476131
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+from tiled.authn_database.orm import Session, JSONVariant
+# revision identifiers, used by Alembic.
+revision = 'c7bd2573716d'
+down_revision = '4a9dfaba4a98'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        Session.__tablename__
+        ,sa.Column('state', JSONVariant, nullable=True))
+
+
+def downgrade():
+    op.drop_column(Session.__tablename__, 'state')
