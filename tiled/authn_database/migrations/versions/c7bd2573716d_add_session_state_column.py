90655f597d3597389640078f4a0f5ed36e69b1b2
Dylan McReynolds <dmcreynolds@lbl.gov>
2023-07-10 15:31:08 -0700

    black updates

--- tiled/authn_database/migrations/versions/c7bd2573716d_add_session_state_column.py
+++ tiled/authn_database/migrations/versions/c7bd2573716d_add_session_state_column.py
@@ -10,18 +10,19 @@ import sqlalchemy as sa
 from sqlalchemy.dialects.postgresql import JSONB
 
 from tiled.authn_database.orm import Session, JSONVariant
+
 # revision identifiers, used by Alembic.
-revision = 'c7bd2573716d'
-down_revision = '4a9dfaba4a98'
+revision = "c7bd2573716d"
+down_revision = "4a9dfaba4a98"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     op.add_column(
-        Session.__tablename__
-        ,sa.Column('state', JSONVariant, nullable=True))
+        Session.__tablename__, sa.Column("state", JSONVariant, nullable=False)
+    )
 
 
 def downgrade():
-    op.drop_column(Session.__tablename__, 'state')
+    op.drop_column(Session.__tablename__, "state")
