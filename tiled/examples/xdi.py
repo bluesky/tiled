2137e606370b4020ea68c1db44c4b814c7b6efa9
Dan Allan <dallan@bnl.gov>
2023-07-25 14:25:14 -0400

    Include version with Spec.

--- tiled/examples/xdi.py
+++ tiled/examples/xdi.py
@@ -6,18 +6,20 @@ Source: https://github.com/XraySpectroscopy/XAS-Data-Interchange
 
 import collections
 import io
-import re
 import pathlib
+import re
 
-import dask
 import pandas as pd
 
 from tiled.adapters.dataframe import DataFrameAdapter
+from tiled.structures.core import Spec
 
 
-def read_xdi(file, metadata=None, specs=None, meta=None, divisions=None, access_policy=None):
+def read_xdi(
+    file, metadata=None, specs=None, meta=None, divisions=None, access_policy=None
+):
     "Read XDI-formatted file given a filepath or a readable buffer."
-    if isinstance(file, (str,pathlib.Path)):
+    if isinstance(file, (str, pathlib.Path)):
         # Treat file as filepath.
         with open(file, "r") as file_:
             return _read_xdi(file_)
@@ -31,9 +33,9 @@ def _read_xdi(f):
     metadata = {}
     fields = collections.defaultdict(dict)
 
-    #if isinstance(f, pathlib.PosixPath):
+    # if isinstance(f, pathlib.PosixPath):
     #    line = f.read_text().split('\n')[0]
-    #else:
+    # else:
     line = f.readline()
     m = re.match(r"#\s*XDI/(\S*)\s*(\S*)?", line)
     if not m:
@@ -90,7 +92,9 @@ def _read_xdi(f):
 
     df = pd.read_table(f, delim_whitespace=True, names=col_labels)
 
-    return DataFrameAdapter.from_pandas(df, npartitions=1, metadata=metadata, specs=["xdi"])
+    return DataFrameAdapter.from_pandas(
+        df, npartitions=1, metadata=metadata, specs=[Spec("xdi", version="1.0")]
+    )
 
 
 def write_xdi(df, metadata):
