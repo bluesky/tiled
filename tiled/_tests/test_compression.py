2095167d35a90acaf028a25a17df44c78ac15dce
Dan Allan <dallan@bnl.gov>
2023-10-02 10:26:10 -0400

    Improve test coverage on compression, and fix bugs (#572)
    * Test compression.
    
    * Update usage for API change.
    
    * Refactor for DRY
    
    * Fix bug (hard-coded instead of using loop var)
    
    * Handle MIME types with parameters.
    
    * whitespace
    
    Co-authored-by: Padraic Shafer <76011594+padraic-shafer@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Padraic Shafer <76011594+padraic-shafer@users.noreply.github.com>

---
+++ tiled/_tests/test_compression.py
@@ -0,0 +1,50 @@
+import numpy
+import pytest
+from starlette.testclient import TestClient
+
+from ..adapters.array import ArrayAdapter
+from ..adapters.mapping import MapAdapter
+from ..server.app import build_app
+
+
+@pytest.fixture
+def app():
+    metadata = {str(i): {str(j): j for j in range(100)} for i in range(100)}
+    tree = MapAdapter(
+        {
+            "compresses_well": ArrayAdapter.from_array(
+                numpy.zeros((1000, 1000)), metadata=metadata
+            )
+        },
+    )
+    return build_app(tree, authentication={"single_user_api_key": "secret"})
+    # In this module we use a raw TestClient instead of tiled.client to omit
+    # tiled.client's default headers and other configuration.
+
+
+def test_gzip_supported(app):
+    with TestClient(app=app) as client:
+        client.headers["Authorization"] = "Apikey secret"
+        client.headers["Accept-Encoding"] = "gzip"
+        metadata_response = client.get("/api/v1/search")
+        data_response = client.get(
+            "/api/v1/array/full/compresses_well", headers={"Accept": "text/csv"}
+        )
+    assert metadata_response.status_code == 200
+    assert data_response.status_code == 200
+    assert "gzip" in metadata_response.headers["Content-Encoding"]
+    assert "gzip" in data_response.headers["Content-Encoding"]
+
+
+def test_zstd_preferred(app):
+    with TestClient(app=app) as client:
+        client.headers["Authorization"] = "Apikey secret"
+        client.headers["Accept-Encoding"] = "zstd"
+        metadata_response = client.get("/api/v1/search")
+        data_response = client.get(
+            "/api/v1/array/full/compresses_well", headers={"Accept": "text/csv"}
+        )
+    assert metadata_response.status_code == 200
+    assert data_response.status_code == 200
+    assert "zstd" in metadata_response.headers["Content-Encoding"]
+    assert "zstd" in data_response.headers["Content-Encoding"]
