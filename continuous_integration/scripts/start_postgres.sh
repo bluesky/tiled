672f9abd461e2cc0ce460ff6e0b38cc7c88929c5
Kari Barry <kezzsim@gmail.com>
2023-12-14 14:34:40 -0500

    Correct mountpoint 🐳

--- continuous_integration/scripts/start_postgres.sh
+++ continuous_integration/scripts/start_postgres.sh
@@ -4,5 +4,5 @@ set -e
 # Try and get the latest postgres test data from the tiled-example-database repository
 wget https://github.com/bluesky/tiled-example-database/releases/latest/download/tiled_test_db_pg.sql
 
-docker run -d --rm --name tiled-test-postgres -p 5432:5432 -e POSTGRES_PASSWORD=secret -v tiled_test_db_pg.sql:/docker-entrypoint-initdb.d/tiled_test_db_pg.sql -d docker.io/postgres
+docker run -d --rm --name tiled-test-postgres -p 5432:5432 -e POSTGRES_PASSWORD=secret -v ./tiled_test_db_pg.sql:/docker-entrypoint-initdb.d/tiled_test_db_pg.sql -d docker.io/postgres
 docker ps
