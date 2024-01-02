97cfc5ddd8251a8365b0552cba68b2aa7ab5548c
Dan Allan <dallan@bnl.gov>
2024-01-02 14:29:07 -0500

    Use curl instead of wget; make script idempotent.

--- continuous_integration/scripts/start_postgres.sh
+++ continuous_integration/scripts/start_postgres.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Try and get the latest postgres test data from the tiled-example-database repository
-wget https://github.com/bluesky/tiled-example-database/releases/latest/download/tiled_test_db_pg.sql
+curl -fsSLO https://github.com/bluesky/tiled-example-database/releases/latest/download/tiled_test_db_pg.sql
 
 docker run -d --rm --name tiled-test-postgres -p 5432:5432 -e POSTGRES_PASSWORD=secret -e POSTGRES_DB=tiled-example-data -v ./tiled_test_db_pg.sql:/docker-entrypoint-initdb.d/tiled_test_db_pg.sql -d docker.io/postgres:16
 docker ps
