35d9ad6261aa09434db86fd9c4a9ba8062677765
Padraic Shafer <pshafer@bnl.gov>
2024-02-14 21:12:20 -0800

    Adjust expected requests for test_url_limit_bypass()

--- tiled/_tests/test_xarray.py
+++ tiled/_tests/test_xarray.py
@@ -196,6 +196,7 @@ def test_url_limit_batching(client):
 def test_url_limit_bypass(client):
     "GET requests beyond the URL length limit should become POST requests."
     expected = EXPECTED["wide"]
+    expected_requests = 2  # Once for data_vars + once for coords
     dsc = client["wide"]
     dsc.read()  # Dry run to run any one-off state-initializing requests.
     # Accumulate Requests here for later inspection.
@@ -206,7 +207,9 @@ def test_url_limit_bypass(client):
 
     client.context.http_client.event_hooks["request"].append(accumulate)
     original = xarray_client.URL_CHARACTER_LIMIT
-    for URL_MAX_LENGTH in numpy.logspace(numpy.log10(original), 2., num=3):
+    ORIGINAL_LIMIT = numpy.log10(original)
+    TINY_LIMIT = numpy.log10(10)
+    for URL_MAX_LENGTH in numpy.logspace(ORIGINAL_LIMIT, TINY_LIMIT, num=3):
         try:
             # It should never be necessary to tune this for real-world use,
             # but we use this knob as a way to test its operation.
@@ -214,7 +217,7 @@ def test_url_limit_bypass(client):
             requests.clear()  # Empty the Request cache.
             actual = dsc.read()
             xarray.testing.assert_equal(actual, expected)
-            assert len(requests) == 1
+            assert len(requests) == expected_requests
         finally:
             # Restore default.
             xarray_client.URL_CHARACTER_LIMIT = original
