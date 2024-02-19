73faa7b35f4bcf51e1db27b5e06e6aff1adb6d54
Padraic Shafer <pshafer@bnl.gov>
2024-02-19 13:34:51 -0800

    Add Zarr and HDF5 to test_container_fields

--- tiled/_tests/test_container_fields.py
+++ tiled/_tests/test_container_fields.py
@@ -3,6 +3,7 @@ import io
 import h5py
 import pandas
 import pytest
+import zarr
 
 from ..catalog import in_memory
 from ..catalog.register import register
@@ -92,6 +93,81 @@ async def test_excel_fields(awaitable_client, fields):
         client.export(buffer, fields=fields, format="application/x-hdf5")
         # TODO: Enable container to export XLSX if all nodes are tables?
         # client.export(buffer, fields=fields, format=XLSX_MIME_TYPE)
+
+    # Spreadsheet contents were fetched as a single request using "full" route
+    for request in history.requests:
+        print(request)
+    (request,) = history.requests
+    request_url_path = request.url.copy_with(query=None)
+    assert request_url_path == url_path
+
+    # Only the requested fields were fetched
+    file = h5py.File(buffer)
+    actual_fields = set(file.keys())
+    expected = set(fields or client.keys())  # By default all fields were fetched
+    assert actual_fields == expected
+
+
+def mark_xfail(value, unsupported="UNSPECIFIED ADAPTER"):
+    "Indicate that this parameterized value is expected to fail"
+    reason = "Tiled does not currently support selecting 'fields' for {unsupported}"
+    return pytest.param(value, marks=pytest.mark.xfail(reason=reason))
+
+
+@pytest.fixture
+def zarr_data_dir(tmpdir):
+    root = zarr.open(str(tmpdir / "zarr_group.zarr"), "w")
+    for i, group in enumerate("abcde"):
+        root.create_dataset(group, data=range(i, i + 3))
+    return tmpdir
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("fields", (None, (), mark_xfail(("b", "d"), "Zarr")))
+@awaitable_client_from_data_dir("zarr_data_dir")
+async def test_zarr_group_fields(awaitable_client, fields):
+    client = await awaitable_client
+    client = client["zarr_group"]
+    url_path = client.item["links"]["full"]
+    buffer = io.BytesIO()
+    with record_history() as history:
+        client.export(buffer, fields=fields, format="application/x-hdf5")
+
+    # Spreadsheet contents were fetched as a single request using "full" route
+    for request in history.requests:
+        print(request)
+    (request,) = history.requests
+    request_url_path = request.url.copy_with(query=None)
+    assert request_url_path == url_path
+
+    # Only the requested fields were fetched
+    file = h5py.File(buffer)
+    actual_fields = set(file.keys())
+    expected = set(fields or client.keys())  # By default all fields were fetched
+    assert actual_fields == expected
+
+
+@pytest.fixture
+def hdf5_data_dir(tmpdir):
+    with h5py.File(str(tmpdir / "hdf5_example.h5"), "w") as file:
+        file["x"] = [1, 2, 3]
+        group = file.create_group("g")
+        group["y"] = [4, 5, 6]
+    return tmpdir
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "fields", (None, (), mark_xfail(("x",), "HDF5"), mark_xfail(("g",), "HDF5"))
+)
+@awaitable_client_from_data_dir("hdf5_data_dir")
+async def test_hdf5_fields(awaitable_client, fields):
+    client = await awaitable_client
+    client = client["hdf5_example"]
+    url_path = client.item["links"]["full"]
+    buffer = io.BytesIO()
+    with record_history() as history:
+        client.export(buffer, fields=fields, format="application/x-hdf5")
 
     # Spreadsheet contents were fetched as a single request using "full" route
     for request in history.requests:
