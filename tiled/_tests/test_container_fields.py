4b6bb4eaaad4493cebcf73156e6c833ffabd914c
Padraic Shafer <pshafer@bnl.gov>
2024-02-19 11:59:33 -0800

    test_excel_fields exports selected sheets

--- tiled/_tests/test_container_fields.py
+++ tiled/_tests/test_container_fields.py
@@ -1,6 +1,7 @@
 import io
 
 import h5py
+import pandas
 import pytest
 
 from ..catalog import in_memory
@@ -23,11 +24,12 @@ def example_data_dir(tmpdir_factory):
 
 
 @pytest.fixture
-async def awaitable_client_generator(example_data_dir):
-    catalog = in_memory(readable_storage=[example_data_dir])
+async def awaitable_client_generator(request: pytest.FixtureRequest):
+    data_dir = request.getfixturevalue(request.param)
+    catalog = in_memory(readable_storage=[data_dir])
     with Context.from_app(build_app(catalog)) as context:
         client = from_context(context)
-        await register(catalog, example_data_dir)
+        await register(catalog, data_dir)
         yield client
 
 
@@ -38,6 +40,9 @@ async def awaitable_client(awaitable_client_generator):
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize("fields", (None, (), ("a", "b")))
+@pytest.mark.parametrize(
+    "awaitable_client_generator", ["example_data_dir"], indirect=True
+)
 async def test_directory_fields(awaitable_client, fields):
     client = await awaitable_client
     url_path = client.item["links"]["full"]
@@ -52,6 +57,44 @@ async def test_directory_fields(awaitable_client, fields):
 
     # Only the requested fields were fetched
     file = h5py.File(buffer, "r")
+    actual_fields = set(file.keys())
+    expected = set(fields or client.keys())  # By default all fields were fetched
+    assert actual_fields == expected
+
+
+@pytest.fixture
+def excel_data_dir(tmpdir):
+    df = pandas.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    with pandas.ExcelWriter(tmpdir / "spreadsheet.xlsx") as writer:
+        for i in range(10):
+            df.to_excel(writer, sheet_name=f"Sheet {i+1}", index=False)
+    return tmpdir
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("fields", (None, (), ("Sheet 1", "Sheet 10")))
+@pytest.mark.parametrize(
+    "awaitable_client_generator", ["excel_data_dir"], indirect=True
+)
+async def test_excel_fields(awaitable_client, fields):
+    client = await awaitable_client
+    client = client["spreadsheet"]
+    url_path = client.item["links"]["full"]
+    buffer = io.BytesIO()
+    with record_history() as history:
+        client.export(buffer, fields=fields, format="application/x-hdf5")
+        # TODO: Enable container to export XLSX if all nodes are tables?
+        # client.export(buffer, fields=fields, format=XLSX_MIME_TYPE)
+
+    # Spreadsheet contents were fetched as a single request using "full" route
+    for request in history.requests:
+        print(request)
+    (request,) = history.requests
+    request_url_path = request.url.copy_with(query=None)
+    assert request_url_path == url_path
+
+    # Only the requested fields were fetched
+    file = h5py.File(buffer)
     actual_fields = set(file.keys())
     expected = set(fields or client.keys())  # By default all fields were fetched
     assert actual_fields == expected
