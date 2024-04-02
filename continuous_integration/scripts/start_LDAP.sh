2d7ab6ac8e237a0ffb0ae60f406453a6ca80669b
Dan Allan <dallan@bnl.gov>
2024-04-02 10:20:39 -0400

    Adjust for backward-incompatible change in Docker 7

--- continuous_integration/scripts/start_LDAP.sh
+++ continuous_integration/scripts/start_LDAP.sh
@@ -3,6 +3,5 @@ set -e
 
 # Start LDAP server in docker container
 docker pull bitnami/openldap:latest
-pip install docker-compose
-docker-compose -f continuous_integration/docker-configs/ldap-docker-compose.yml up -d
+docker compose -f continuous_integration/docker-configs/ldap-docker-compose.yml up -d
 docker ps
