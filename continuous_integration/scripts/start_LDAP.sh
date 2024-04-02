6fd38fd56d71e4f14a1e112e57a39eb8eb44e898
Dan Allan <dallan@bnl.gov>
2024-04-02 09:21:22 -0400

    CI: Ensure docker-compose is installed

--- continuous_integration/scripts/start_LDAP.sh
+++ continuous_integration/scripts/start_LDAP.sh
@@ -3,5 +3,6 @@ set -e
 
 # Start LDAP server in docker container
 docker pull bitnami/openldap:latest
+pip install docker-compose
 docker-compose -f continuous_integration/docker-configs/ldap-docker-compose.yml up -d
 docker ps
