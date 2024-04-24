4227818eaa82d16ab6e4d9e8636f1a317ef50652
Dan Allan <dallan@bnl.gov>
2024-04-24 16:51:22 -0400

    Do not assume 'content-encoding' header is set. (#727)

--- tiled/server/metrics.py
+++ tiled/server/metrics.py
@@ -125,13 +125,14 @@ def capture_request_metrics(request, response):
             metrics["read"]["pack"]
         )
     if "compress" in metrics:
-        encoding = response.headers["content-encoding"]
-        COMPRESSION_DURATION.labels(
-            method=method, code=code, endpoint=endpoint, encoding=encoding
-        ).observe(metrics["compress"]["dur"])
-        COMPRESSION_RATIO.labels(
-            method=method, code=code, endpoint=endpoint, encoding=encoding
-        ).observe(metrics["compress"]["ratio"])
+        encoding = response.headers.get("content-encoding")
+        if encoding:
+            COMPRESSION_DURATION.labels(
+                method=method, code=code, endpoint=endpoint, encoding=encoding
+            ).observe(metrics["compress"]["dur"])
+            COMPRESSION_RATIO.labels(
+                method=method, code=code, endpoint=endpoint, encoding=encoding
+            ).observe(metrics["compress"]["ratio"])
 
 
 @lru_cache()
