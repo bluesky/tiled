bae187f31201973e071296addd1cee67c953f79a
Dan Allan <dallan@bnl.gov>
2024-05-06 15:47:25 -0400

    Test externally managed container format.

--- tiled/_tests/test_sync.py
+++ tiled/_tests/test_sync.py
@@ -2,6 +2,7 @@ import asyncio
 import contextlib
 import tempfile
 
+import h5py
 import numpy
 import pandas
 import tifffile
@@ -16,9 +17,9 @@ from tiled.server.app import build_app
 
 
 @contextlib.contextmanager
-def client_factory():
+def client_factory(readable_storage=None):
     with tempfile.TemporaryDirectory() as tempdir:
-        catalog = in_memory(writable_storage=tempdir)
+        catalog = in_memory(writable_storage=tempdir, readable_storage=readable_storage)
         app = build_app(catalog)
         with Context.from_app(app) as context:
             client = from_context(context)
@@ -37,6 +38,11 @@ def populate_external(client, tmp_path):
     for filepath in [tmp_path / "table.csv", subdir / "nested_table.csv"]:
         with open(filepath, "w") as file:
             file.write("x,y\n1,2\n3,4\n")
+    # container
+    for filepath in [tmp_path / "group.h5", subdir / "group.h5"]:
+        with h5py.File(filepath, "w") as file:
+            g = file.create_group("g")
+            g["data"] = numpy.arange(3)
     asyncio.run(register(client, tmp_path))
 
 
@@ -66,7 +72,7 @@ def test_sync_internal():
 
 
 def test_sync_external(tmp_path):
-    with client_factory() as dest:
+    with client_factory(readable_storage=[tmp_path]) as dest:
         with client_factory() as source:
             populate_external(source, tmp_path)
             sync(source, dest)
