daaa8f3dd3386d66eac6f2f04993f8493f36eafa
Dan Allan <dallan@bnl.gov>
2024-06-18 12:55:48 -0400

    Fix Context repr when authenticated as service

--- tiled/client/context.py
+++ tiled/client/context.py
@@ -168,9 +168,14 @@ class Context:
             if self.server_info["authentication"].get("links"):
                 whoami = self.whoami()
                 auth_info.append("as")
-                auth_info.append(
-                    ",".join(f"'{identity['id']}'" for identity in whoami["identities"])
-                )
+                if whoami["type"] == "service":
+                    auth_info.append(f"service '{whoami['uuid']}'")
+                else:
+                    auth_info.append(
+                        ",".join(
+                            f"'{identity['id']}'" for identity in whoami["identities"]
+                        )
+                    )
             if self.api_key is not None:
                 auth_info.append(
                     f"with API key '{self.api_key[:min(len(self.api_key)//2, 8)]}...'"
