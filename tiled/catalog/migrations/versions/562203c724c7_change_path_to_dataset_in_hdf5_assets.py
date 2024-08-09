161fea5c44821b6399f1b3071b58abcec529f1f1
Eugene M <ymatviych@bnl.gov>
2024-08-09 16:22:37 -0400

    MNT: format and lint

--- tiled/catalog/migrations/versions/562203c724c7_change_path_to_dataset_in_hdf5_assets.py
+++ tiled/catalog/migrations/versions/562203c724c7_change_path_to_dataset_in_hdf5_assets.py
@@ -5,15 +5,17 @@ Revises: ed3a4223a600
 Create Date: 2024-08-09 10:13:36.384838
 
 """
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
+
 from tiled.catalog.orm import JSONVariant
 
 # revision identifiers, used by Alembic.
-revision = '562203c724c7'
-down_revision = 'ed3a4223a600'
+revision = "562203c724c7"
+down_revision = "ed3a4223a600"
 branch_labels = None
 depends_on = None
+
 
 def upgrade():
     connection = op.get_bind()
@@ -26,12 +28,19 @@ def upgrade():
 
     # Loop over all nodes that have 'parameters' field, choose and update those related to hdf5 files
     condition = sa.text("CAST(nodes.metadata->>'parameters' AS TEXT) != ''")
-    cursor = connection.execute(sa.select(nodes.c.id, nodes.c.metadata).filter(condition).select_from(nodes))
+    cursor = connection.execute(
+        sa.select(nodes.c.id, nodes.c.metadata).filter(condition).select_from(nodes)
+    )
     for _id, _md in cursor:
-        if 'hdf5' in (_md.get('mimetype', '') + _md.get('spec', '')).lower():
-                if isinstance(_md['parameters'], dict) and ('path' in _md['parameters'].keys()):
-                    _md['parameters']['dataset'] = _md['parameters'].pop('path')
-                    connection.execute(nodes.update().where(nodes.c.id == _id).values(metadata=_md))
+        if "hdf5" in (_md.get("mimetype", "") + _md.get("spec", "")).lower():
+            if isinstance(_md["parameters"], dict) and (
+                "path" in _md["parameters"].keys()
+            ):
+                _md["parameters"]["dataset"] = _md["parameters"].pop("path")
+                connection.execute(
+                    nodes.update().where(nodes.c.id == _id).values(metadata=_md)
+                )
+
 
 def downgrade():
     # This _could_ be implemented but we will wait for a need since we are
