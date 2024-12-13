08665aeb69ea0e77b4dd5053f9a0b6134c93cedf
Dan Allan <dallan@bnl.gov>
2024-12-13 13:09:48 -0500

    Fix import

--- tiled/adapters/sql.py
+++ tiled/adapters/sql.py
@@ -12,9 +12,9 @@ import pyarrow.fs
 from ..structures.core import Spec, StructureFamily
 from ..structures.data_source import Asset, DataSource, Storage
 from ..structures.table import TableStructure
+from ..type_alliases import JSON
 from .array import ArrayAdapter
 from .protocols import AccessPolicy
-from .type_alliases import JSON
 
 
 class SQLAdapter:
