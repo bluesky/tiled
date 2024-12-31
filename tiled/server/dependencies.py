6a74493cc75facdfb25c05eaf11f3b579256382a
Eugene M <ymatviych@bnl.gov>
2024-12-31 17:12:48 -0500

    ENH: handle part query param

--- tiled/server/dependencies.py
+++ tiled/server/dependencies.py
@@ -154,11 +154,13 @@ def SecureEntry(scopes, structure_families=None):
             raise HTTPException(
                 status_code=HTTP_404_NOT_FOUND, detail=f"No such entry: {path_parts}"
             )
+
         # Fast path for the common successful case
         if (structure_families is None) or (
             entry.structure_family in structure_families
         ):
             return entry
+
         # Handle consolidated structure_family
         if entry.structure_family == StructureFamily.consolidated:
             if not part:
