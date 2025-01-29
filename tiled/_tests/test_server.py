a8b05e5f5f5f9c0bfa7084602ae5f4196ff711d5
Dan Allan <dallan@bnl.gov>
2025-01-29 09:55:31 -0500

    Ensure back-compat with old Python clients

--- tiled/_tests/test_server.py
+++ tiled/_tests/test_server.py
@@ -1,15 +1,21 @@
 import contextlib
+import subprocess
+import sys
 import threading
 import time
 
+import httpx
+import numpy
 import pytest
 import uvicorn
 from fastapi import APIRouter
 from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
 
+from ..adapters.array import ArrayAdapter
+from ..adapters.mapping import MapAdapter
 from ..catalog import in_memory
 from ..client import from_uri
-from ..server.app import build_app
+from ..server.app import build_app, build_app_from_config
 from ..server.logging_config import LOGGING_CONFIG
 
 router = APIRouter()
@@ -67,6 +73,49 @@ def public_server(tmpdir):
         yield url
 
 
+arr = ArrayAdapter.from_array(numpy.ones((5, 5)))
+tree = MapAdapter({"A1": arr, "A2": arr})
+
+
+@pytest.fixture
+def multiuser_server(tmpdir):
+    database_uri = f"sqlite:///{tmpdir}/tiled.sqlite"
+    subprocess.run(
+        [sys.executable, "-m", "tiled", "admin", "initialize-database", database_uri],
+        check=True,
+        capture_output=True,
+    )
+    config = {
+        "authentication": {
+            "secret_keys": ["SECRET"],
+            "providers": [
+                {
+                    "provider": "toy",
+                    "authenticator": "tiled.authenticators:DictionaryAuthenticator",
+                    "args": {
+                        "users_to_passwords": {"alice": "secret1", "bob": "secret2"}
+                    },
+                }
+            ],
+        },
+        "database": {
+            "uri": database_uri,
+        },
+        "trees": [
+            {
+                "tree": f"{__name__}:tree",
+                "path": "/",
+            },
+        ],
+    }
+    app = build_app_from_config(config)
+    app.include_router(router)
+    config = uvicorn.Config(app, port=0, loop="asyncio", log_config=LOGGING_CONFIG)
+    server = Server(config)
+    with server.run_in_thread() as url:
+        yield url
+
+
 @router.get("/error")
 def error():
     1 / 0  # error!
@@ -97,3 +146,25 @@ def test_writing_integration(server):
 
 def test_public_server(public_server):
     from_uri(public_server)
+
+
+def test_internal_authentication_mode_with_password_clients(multiuser_server):
+    "The 'internal' authentication mode used to be named 'password'."
+    # Mock old client
+    response = httpx.get(
+        multiuser_server + "/api/v1/", headers={"user-agent": "python-tiled/0.1.0b16"}
+    )
+    actual_mode = response.json()["authentication"]["providers"][0]["mode"]
+    assert actual_mode == "password"
+
+    # Mock new client
+    response = httpx.get(
+        multiuser_server + "/api/v1/", headers={"user-agent": "python-tiled/0.1.0b17"}
+    )
+    actual_mode = response.json()["authentication"]["providers"][0]["mode"]
+    assert actual_mode == "internal"
+
+    # Mock unknown client
+    response = httpx.get(multiuser_server + "/api/v1/", headers={})
+    actual_mode = response.json()["authentication"]["providers"][0]["mode"]
+    assert actual_mode == "internal"
