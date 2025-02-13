781fb131649fecc599cb4d8544863d6ece26e6b8
Dan Allan <daniel.b.allan@gmail.com>
2025-02-13 13:19:56 -0500

    Remove more needless conversions
    Co-authored-by: Eugene <ymatviych@bnl.gov>

--- tiled/adapters/parquet.py
+++ tiled/adapters/parquet.py
@@ -89,7 +89,7 @@ class ParquetDatasetAdapter:
 
         """
         data_source = copy.deepcopy(data_source)  # Do not mutate caller input.
-        data_uri = str(storage.get("filesystem")) + "".join(
+        data_uri = storage.get("filesystem") + "".join(
             f"/{quote_plus(segment)}" for segment in path_parts
         )
         directory = path_from_uri(data_uri)
