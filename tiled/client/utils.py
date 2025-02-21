1c488ac9cd6b6daaff7db239fa6381a7aa0854f2
Dan Allan <dallan@bnl.gov>
2025-02-21 13:53:49 -0500

    Update import

--- tiled/client/utils.py
+++ tiled/client/utils.py
@@ -359,7 +359,7 @@ def gen_tree(tree, nodes=None, last=None):
     # when this function is used in a CLI where import overhead can accumulate to
     # about 2 seconds, the bulk of the time. Therefore, we do something a bit
     # "clever" here to override the normal structure clients with dummy placeholders.
-    from .client.container import Container
+    from .container import Container
 
     def dummy_client(*args, **kwargs):
         return None
