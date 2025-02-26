da8093c0c1103977263cc4dbaa6589dae09464f3
skarakuzu <karakuzu.seher@gmail.com>
2025-02-26 16:56:36 -0500

    progress in psql and duckdb test additions

--- tiled/_tests/adapters/test_sql_types.py
+++ tiled/_tests/adapters/test_sql_types.py
@@ -2,7 +2,10 @@ import os
 from pathlib import Path
 from typing import AsyncGenerator, Generator
 
-import adbc_driver_sqlite
+import adbc_driver_duckdb
+import adbc_driver_postgresql
+
+# import adbc_driver_sqlite
 import pyarrow as pa
 import pytest
 import pytest_asyncio
@@ -19,6 +22,7 @@ async def postgres_uri() -> AsyncGenerator[str, None]:
 
     async with temp_postgres(uri) as uri_with_database_name:
         yield uri_with_database_name
+        # yield uri_with_database_name.rsplit("/", 1)[0]
 
 
 @pytest.fixture
@@ -31,15 +35,43 @@ def duckdb_uri(tmp_path: Path) -> Generator[str, None, None]:
     yield f"duckdb:///{tmp_path}/test.db"
 
 
-def test_sql_data_types(sqlite_uri: str) -> None:
+def test_duckdb_data_types(duckdb_uri: str) -> None:
     schema = pa.schema([("some_int", pa.int32()), ("some_string", pa.string())])
-    query = arrow_schema_to_create_table(schema, "random_test_table", "sqlite")
+    query = arrow_schema_to_create_table(schema, "random_test_table", "duckdb")
+
     print("querry here", query)
     assert query.find("INTEGER")
     assert query.find("TEXT")
 
-    conn = create_connection(sqlite_uri)
-    assert isinstance(conn, adbc_driver_sqlite.dbapi.Connection)
+    conn = create_connection(duckdb_uri)
+    assert isinstance(conn, adbc_driver_duckdb.dbapi.Connection)
+
+    with conn.cursor() as cursor:
+        cursor.execute("DROP TABLE IF EXISTS random_test_table")
+        cursor.execute(query)
+    conn.commit()
+
+    print("the out", conn.adbc_get_table_schema("random_test_table"))
+    assert conn.adbc_get_table_schema("random_test_table") == pa.schema(
+        [
+            ("some_int", "int32"),
+            ("some_string", "string"),
+        ]
+    )
+    conn.close()
+
+
+def test_psql_data_types(postgres_uri: str) -> None:
+    print("uri", postgres_uri)
+    schema = pa.schema([("some_int", pa.int32()), ("some_string", pa.string())])
+    query = arrow_schema_to_create_table(schema, "random_test_table", "postgresql")
+
+    print("querry here", query)
+    assert query.find("INTEGER")
+    assert query.find("TEXT")
+
+    conn = create_connection(postgres_uri)
+    assert isinstance(conn, adbc_driver_postgresql.dbapi.Connection)
 
     with conn.cursor() as cursor:
         cursor.execute("DROP TABLE IF EXISTS random_test_table")
