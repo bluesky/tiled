58a6b37bef8bcdd629964f87946e944dad31953c
skarakuzu <karakuzu.seher@gmail.com>
2025-02-27 13:07:37 -0500

    added psql  datatype tests

--- tiled/_tests/adapters/test_sql_types.py
+++ tiled/_tests/adapters/test_sql_types.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-from typing import AsyncGenerator, Generator
+from typing import AsyncGenerator, Generator, List
 
 import adbc_driver_duckdb
 import adbc_driver_postgresql
@@ -61,14 +61,109 @@ def test_duckdb_data_types(duckdb_uri: str) -> None:
     conn.close()
 
 
-def test_psql_data_types(postgres_uri: str) -> None:
-    print("uri", postgres_uri)
-    schema = pa.schema([("some_int", pa.int32()), ("some_string", pa.string())])
-    query = arrow_schema_to_create_table(schema, "random_test_table", "postgresql")
+@pytest.mark.parametrize(
+    "actual_schema, expected_schema, expected_keywords",
+    [
+        (
+            pa.schema([("some_bool", pa.bool_())]),
+            pa.schema([("some_bool", "bool")]),
+            ["BOOLEAN"],
+        ),
+        (
+            pa.schema([("some_int8", pa.int8())]),
+            pa.schema([("some_int8", "int16")]),
+            ["SMALLINT"],
+        ),
+        (
+            pa.schema([("some_uint8", pa.uint8())]),
+            pa.schema([("some_uint8", "int16")]),
+            ["SMALLINT"],
+        ),
+        (
+            pa.schema([("some_int16", pa.int16())]),
+            pa.schema([("some_int16", "int16")]),
+            ["SMALLINT"],
+        ),
+        (
+            pa.schema([("some_uint16", pa.uint16())]),
+            pa.schema([("some_uint16", "int16")]),
+            ["SMALLINT"],
+        ),
+        (
+            pa.schema([("some_int32", pa.int32())]),
+            pa.schema([("some_int32", "int32")]),
+            ["INTEGER"],
+        ),
+        (
+            pa.schema([("some_uint32", pa.uint32())]),
+            pa.schema([("some_uint32", "int32")]),
+            ["INTEGER"],
+        ),
+        (
+            pa.schema([("some_int64", pa.int64())]),
+            pa.schema([("some_int64", "int64")]),
+            ["BIGINT"],
+        ),
+        (
+            pa.schema([("some_uint64", pa.uint64())]),
+            pa.schema([("some_uint64", "int64")]),
+            ["BIGINT"],
+        ),
+        (
+            pa.schema([("some_float16", pa.float16())]),
+            pa.schema([("some_float16", "float")]),
+            ["REAL"],
+        ),
+        (
+            pa.schema([("some_float32", pa.float32())]),
+            pa.schema([("some_float32", "float")]),
+            ["REAL"],
+        ),
+        (
+            pa.schema([("some_float64", pa.float64())]),
+            pa.schema([("some_float64", "double")]),
+            ["DOUBLE PRECISION"],
+        ),
+        (
+            pa.schema([("some_string", pa.string())]),
+            pa.schema([("some_string", "string")]),
+            ["TEXT"],
+        ),
+        (
+            pa.schema([("some_large_string", pa.large_string())]),
+            pa.schema([("some_large_string", "string")]),
+            ["TEXT"],
+        ),
+        (
+            pa.schema([("some_integer_array", pa.list_(pa.int32()))]),
+            pa.schema([("some_integer_array", pa.list_(pa.int32()))]),
+            ["INTEGER ARRAY"],
+        ),
+        (
+            pa.schema([("some_large_integer_array", pa.list_(pa.int64()))]),
+            pa.schema([("some_large_integer_array", pa.list_(pa.int64()))]),
+            ["BIGINT ARRAY"],
+        ),
+        (
+            pa.schema([("some_fixed_size_integer_array", pa.list_(pa.int64(), 2))]),
+            pa.schema([("some_fixed_size_integer_array", pa.list_(pa.int64()))]),
+            ["BIGINT ARRAY"],
+        ),
+    ],
+)
+def test_psql_data_types(
+    actual_schema: pa.Schema,
+    expected_schema: pa.Schema,
+    expected_keywords: List[str],
+    postgres_uri: str,
+) -> None:
+    query = arrow_schema_to_create_table(
+        actual_schema, "random_test_table", "postgresql"
+    )
 
     print("querry here", query)
-    assert query.find("INTEGER")
-    assert query.find("TEXT")
+    for keyword in expected_keywords:
+        assert keyword in query
 
     conn = create_connection(postgres_uri)
     assert isinstance(conn, adbc_driver_postgresql.dbapi.Connection)
@@ -79,10 +174,6 @@ def test_psql_data_types(postgres_uri: str) -> None:
     conn.commit()
 
     print("the out", conn.adbc_get_table_schema("random_test_table"))
-    assert conn.adbc_get_table_schema("random_test_table") == pa.schema(
-        [
-            ("some_int", "int32"),
-            ("some_string", "string"),
-        ]
-    )
+    assert conn.adbc_get_table_schema("random_test_table") == expected_schema
+
     conn.close()
