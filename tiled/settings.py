8a156b0cfc469e95659931fa941da42180ae167d
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2025-03-07 11:23:13 +0000

    Move server settings

--- tiled/settings.py
+++ tiled/settings.py
@@ -1,6 +1,11 @@
-from typing import Optional, Union
+import os
+import secrets
+from abc import ABC
+from datetime import timedelta
+from typing import Any, List, Optional, Union
 
 from pydantic import BaseModel, Field
+from pydantic.dataclasses import dataclass
 from pydantic_settings import BaseSettings, CliApp, CliSubCommand, SettingsConfigDict
 
 from tiled.client.context import Context
@@ -65,8 +70,72 @@ class WhoAmI(BaseModel):
             print(",".join(identity["id"] for identity in whoami["identities"]))
 
 
+class Server:
+    ...
+
+
+class API(BaseModel, ABC):
+    enabled: bool = False
+
+    def serve_api(server: Server):
+        ...
+
+
+class FastAPI(API):
+    ...
+
+
+class GraphQL(API):
+    ...
+
+
+# hashable cache key for use in tiled.authn_database.connection_pool
+@dataclass(unsafe_hash=True)
+class DatabaseSettings:
+    uri: Optional[str] = None
+    pool_size: int = 5
+    pool_pre_ping: bool = True
+    max_overflow: int = 5
+
+
+class Serve(BaseModel):
+    tree: Any = None
+    allow_anonymous_access: bool = False
+    allow_origins: List[str] = Field(default_factory=list)
+    authenticator: Any = None
+    # These 'single user' settings are only applicable if authenticator is None.
+    single_user_api_key: str = secrets.token_hex(32)
+    single_user_api_key_generated: bool = "TILED_SINGLE_USER_API_KEY" not in os.environ
+    # The TILED_SERVER_SECRET_KEYS may be a single key or a ;-separated list of
+    # keys to support key rotation. The first key will be used for encryption. Each
+    # key will be tried in turn for decryption.
+    secret_keys: List[str] = [secrets.token_hex(32)]
+    access_token_max_age: timedelta = 15 * 60  # 15 minutes
+    refresh_token_max_age: timedelta = 7 * 24 * 60 * 60  # 7 days
+    session_max_age: timedelta = 365 * 24 * 60 * 60  # 365 days
+    # Put a fairly low limit on the maximum size of one chunk, keeping in mind
+    # that data should generally be chunked. When we implement async responses,
+    # we can raise this global limit.
+    response_bytesize_limit: int = 300_000_000  # 300 MB
+    reject_undeclared_specs: bool = False
+    database_settings: DatabaseSettings = Field(DatabaseSettings(), alias="database")
+    database_init_if_not_exists: bool = False
+    expose_raw_assets: bool = True
+
+    apis: list[API] = [FastAPI(enabled=True)]
+
+    model_config = SettingsConfigDict(
+        env_prefix="TILED_", nested_model_default_partial_update=True
+    )
+
+    def cli_cmd(self) -> None:
+        server = None
+        for api in self.apis:
+            api.serve_api(server)
+
+
 class Settings(BaseSettings):
-    command: CliSubCommand[Union[Login, Logout, WhoAmI]]
+    command: CliSubCommand[Union[Login, Logout, WhoAmI, Serve]]
 
     model_config = SettingsConfigDict(
         env_prefix="TILED_",
