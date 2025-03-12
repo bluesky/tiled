f73c8f348204eaeb9328146f2bce1cb017129347
Eugene M <ymatviych@bnl.gov>
2025-03-12 15:20:42 -0400

    MNT: DB migration

---
+++ tiled/catalog/migrations/versions/45a702586b2a_add_composite_structure_family.py
@@ -0,0 +1,32 @@
+"""Add 'composite' structure family to the structurefamily enum.
+
+Revision ID: 45a702586b2a
+Revises: ed3a4223a600
+Create Date: 2025-03-12 15:05:25.335010
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '45a702586b2a'
+down_revision = 'ed3a4223a600'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    connection = op.get_bind()
+
+    if connection.engine.dialect.name == "postgresql":
+        with op.get_context().autocommit_block():
+            op.execute(
+                sa.text(
+                    "ALTER TYPE structurefamily ADD VALUE IF NOT EXISTS 'composite' AFTER 'awkward'"
+                )
+            )
+
+
+def downgrade():
+    raise NotImplementedError
