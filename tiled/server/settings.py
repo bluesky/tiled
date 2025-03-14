e12e42e7e533065724f688435093e7f6c26f76aa
Ware, Joseph (DLSLtd,RAL,LSCI) <joseph.ware@diamond.ac.uk>
2025-03-14 12:33:10 +0000

    Refactor parsing command line options

--- tiled/server/settings.py
+++ tiled/server/settings.py
@@ -1,4 +1,3 @@
-import os
 import secrets
 from datetime import timedelta
 from functools import cache
@@ -25,7 +24,6 @@ class Settings(BaseSettings):
     authenticator: Any = None
     # These 'single user' settings are only applicable if authenticator is None.
     single_user_api_key: str = secrets.token_hex(32)
-    single_user_api_key_generated: bool = "TILED_SINGLE_USER_API_KEY" not in os.environ
     # The TILED_SERVER_SECRET_KEYS may be a single key or a ;-separated list of
     # keys to support key rotation. The first key will be used for encryption. Each
     # key will be tried in turn for decryption.
