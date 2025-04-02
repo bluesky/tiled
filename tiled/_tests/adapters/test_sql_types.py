849db7deb26606fa3f3932fe85e5ea7d09cd8676
Dan Allan <dallan@bnl.gov>
2025-04-02 12:27:55 -0400

    Refactor to parametrize test over dialects.

--- tiled/_tests/adapters/test_sql_types.py
+++ tiled/_tests/adapters/test_sql_types.py
@@ -1,12 +1,9 @@
 import os
 from pathlib import Path
-from typing import AsyncGenerator, Generator, List
+from typing import AsyncGenerator, Generator, List, Literal
 
-import adbc_driver_duckdb
 import adbc_driver_postgresql
 import adbc_driver_sqlite
-
-# import adbc_driver_sqlite
 import pyarrow as pa
 import pytest
 import pytest_asyncio
@@ -20,7 +17,7 @@ from tiled.adapters.sql import (
 
 
 @pytest_asyncio.fixture
-async def postgres_uri() -> AsyncGenerator[str, None]:
+async def postgresql_uri() -> AsyncGenerator[str, None]:
     uri = os.getenv("TILED_TEST_POSTGRESQL_URI")
     if uri is None:
         pytest.skip("TILED_TEST_POSTGRESQL_URI is not set")
@@ -30,16 +27,17 @@ async def postgres_uri() -> AsyncGenerator[str, None]:
         # yield uri_with_database_name.rsplit("/", 1)[0]
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 def sqlite_uri(tmp_path: Path) -> Generator[str, None, None]:
     yield f"sqlite:///{tmp_path}/test.db"
 
 
-@pytest.fixture
+@pytest_asyncio.fixture
 def duckdb_uri(tmp_path: Path) -> Generator[str, None, None]:
     yield f"duckdb:///{tmp_path}/test.db"
 
 
+@pytest.mark.parametrize("dialect", ["duckdb"])
 @pytest.mark.parametrize(
     "actual_schema, expected_schema, expected_typedefs",
     [
@@ -135,19 +133,20 @@ def duckdb_uri(tmp_path: Path) -> Generator[str, None, None]:
         ),
     ],
 )
-def test_duckdb_data_types(
+def test_data_types(
     actual_schema: pa.Schema,
     expected_schema: pa.Schema,
     expected_typedefs: List[str],
-    duckdb_uri: str,
+    dialect: Literal["postgresql", "sqlite", "duckdb"],
+    request: pytest.FixtureRequest,
 ) -> None:
-    columns = arrow_schema_to_column_defns(actual_schema, "duckdb")
+    db_uri = request.getfixturevalue(f"{dialect}_uri")
+    columns = arrow_schema_to_column_defns(actual_schema, dialect)
     assert expected_typedefs == list(columns.values())
 
-    query = arrow_schema_to_create_table(actual_schema, "random_test_table", "duckdb")
+    query = arrow_schema_to_create_table(actual_schema, "random_test_table", dialect)
 
-    conn = create_connection(duckdb_uri)
-    assert isinstance(conn, adbc_driver_duckdb.dbapi.Connection)
+    conn = create_connection(db_uri)
 
     with conn.cursor() as cursor:
         cursor.execute("DROP TABLE IF EXISTS random_test_table")
