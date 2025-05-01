74835ef513f0dfffa173cb247c967430d76ead6d
nmaytan <nmaytan@bnl.gov>
2025-05-01 03:57:09 -0400

    Add API sync scheduler, make public take inheritable

--- authz_workspace/tag_based_access_policy.py
+++ authz_workspace/tag_based_access_policy.py
@@ -1,5 +1,8 @@
+import asyncio
 import grp
+import threading
 import warnings
+from datetime import datetime, timedelta
 from pathlib import Path
 from sys import intern
 
@@ -11,22 +14,133 @@ from access_blob_queries import AccessBlobFilter
 from tiled.access_policies import NO_ACCESS
 from tiled.scopes import ALL_SCOPES
 
+TILED_TBAP_SYNC_CURRENT_CYCLE_PERIOD = 1  # minutes
+TILED_TBAP_SYNC_ALL_CYCLES_PERIOD = 5  # minutes
+
+TILED_TBAP_GROUP_CACHE_MAXSIZE = 55_000
+TILED_TBAP_GROUP_CACHE_TTL = 3600  # seconds
+
+_TAG_SYNC_TICK_RATE = 15  # seconds
 _MAX_TAG_NESTING = 5
 
 WRITING_SERVICE_ACCOUNT_UUIDS = {}
 
-group_record_cache = cachetools.TTLCache(maxsize=50_000, ttl=14_400)
+group_record_cache = cachetools.TTLCache(
+    maxsize=TILED_TBAP_GROUP_CACHE_MAXSIZE, ttl=TILED_TBAP_GROUP_CACHE_TTL
+)
+
+TAG_SYNC_LOCK = asyncio.Lock()
+sync_tags_tasks = []
+
+
+if __debug__:
+    import logging
+    import os
+
+    logger = logging.getLogger(__name__)
+    handler = logging.StreamHandler()
+    handler.setLevel("DEBUG")
+    handler.setFormatter(logging.Formatter("TAG BASED ACCESS POLICY: %(message)s"))
+    logger.addHandler(handler)
+
+    log_level = os.getenv("TAG_BASED_ACCESS_POLICY_LOG_LEVEL")
+    if log_level:
+        logger.setLevel(log_level.upper())
+
+
+def calculate_first_cycle(period):
+    # calculate first run using midnight as reference point
+    now = datetime.now()
+    midnight = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    minutes_since_midnight = (now - midnight).total_seconds() // 60
+    minutes_until_cycle = period - (minutes_since_midnight % period)
+    next_cycle_at = now + timedelta(minutes=minutes_until_cycle)
+    return next_cycle_at
+
+
+def create_sync_tags_tasks(coro, *args, period=1, **kwargs):
+    next_cycle_at = calculate_first_cycle(period)
+    sync_tags_tasks.append(
+        {
+            "coro": (coro, args, kwargs),
+            "rate": (period // 60, period % 60),
+            "next_run": (next_cycle_at.hour, next_cycle_at.minute),
+            "last_run": None,
+        }
+    )
+
+
+async def _sync_tags_scheduler():
+    loop = asyncio.get_running_loop()
+
+    # this logic normalizes update ticks to even wall-clock times.
+    # this way, the schedule is predictable regardless of when
+    # the application starts.
+    now = datetime.now()
+    tick_rate = _TAG_SYNC_TICK_RATE
+    tick_rate = min(3600, tick_rate)  # this logic works up to 1 hour tick
+    seconds_since_hour = (now.minute * 60) + now.second
+    cycles_since_hour = seconds_since_hour // tick_rate  # floor div
+    last_cycle_seconds = cycles_since_hour * tick_rate
+    next_cycle_seconds = last_cycle_seconds + tick_rate
+    next_cycle_at = now.replace(minute=0, second=0, microsecond=0) + timedelta(
+        seconds=next_cycle_seconds
+    )
+    next_tick = loop.time() + (next_cycle_at - now).total_seconds()
+
+    while True:
+        await asyncio.sleep(max(0, next_tick - loop.time()))
+        now = datetime.now()
+        logger.debug(f"Tag Sync Event Loop Tick {now}")
+        for task in sync_tags_tasks:
+            if task["last_run"] == (now.hour, now.minute):
+                continue
+
+            coro, args, kwargs = task["coro"]
+            hour, minute = task["next_run"]
+            if task["last_run"] is None:
+                loop.create_task(coro(*args, **kwargs))
+                task["last_run"] = (now.hour, now.minute)
+                logger.debug("Initial run of coroutines")
+            elif (now.minute >= minute) and (now.hour >= hour):
+                loop.create_task(coro(*args, **kwargs))
+                hours, minutes = task["rate"]
+                task["next_run"] = (
+                    (hour + hours + ((minute + minutes) // 60)) % 24,
+                    (minute + minutes) % 60,
+                )
+                task["last_run"] = (now.hour, now.minute)
+                logger.debug(
+                    f"""Last run of '{coro.__name__}': {task["last_run"]} (hour, min)"""
+                )
+                logger.debug(
+                    f"""Next run of '{coro.__name__}': {task["next_run"]} (hour, min)"""
+                )
+        next_tick += tick_rate
+
+
+def _start_sync_tags_loop(loop: asyncio.AbstractEventLoop):
+    asyncio.set_event_loop(loop)
+    loop.create_task(_sync_tags_scheduler())
+    loop.run_forever()
+
+
+sync_tags_loop = asyncio.new_event_loop()
+thread_sync_tags = threading.Thread(
+    target=_start_sync_tags_loop, args=(sync_tags_loop,), daemon=True
+)
+thread_sync_tags.start()
 
 
 def get_group_users(groupname):
     try:
         usernames = group_record_cache[groupname]
     except KeyError:
-        # logger.debug("%s: Cache miss", username)
+        logger.debug("%s: Cache miss", groupname)
         usernames = grp.getgrnam(groupname).gr_mem
         group_record_cache[groupname] = usernames
     else:
-        # logger.debug("%s: Cache hit", username)
+        logger.debug("%s: Cache hit", groupname)
         pass
 
     return usernames
@@ -48,7 +162,7 @@ class TagBasedAccessPolicy:
     def __init__(self, *, provider, tag_config, url, scopes=None):
         self.provider = provider
         self.tag_config_path = Path(tag_config)
-        self.client = httpx.Client(base_url=url)
+        self.client = httpx.AsyncClient(base_url=url)
         self.scopes = scopes if (scopes is not None) else ALL_SCOPES
         self.read_scopes = [intern("read:metadata"), intern("read:data")]
         self.reverse_lookup_scopes = [intern("read:metadata"), intern("read:data")]
@@ -60,9 +174,11 @@ class TagBasedAccessPolicy:
         self.tags = {}
         self.tag_owners = {}
         self.compiled_tags = {}
+        self.compiled_public = set({self.public_tag})
         self.compiled_scopes = {}
         self.compiled_tag_owners = {}
         self.loaded_tags = {}
+        self.loaded_public = set()
         self.loaded_scopes = {}
         self.loaded_tag_owners = {}
 
@@ -70,6 +186,13 @@ class TagBasedAccessPolicy:
         self.create_tags_root_node()
         self.compile()
         self.load_compiled_tags()
+
+        create_sync_tags_tasks(
+            self.reload_tags_all_cycles, period=TILED_TBAP_SYNC_ALL_CYCLES_PERIOD
+        )
+        create_sync_tags_tasks(
+            self.reload_tags_current_cycle, period=TILED_TBAP_SYNC_CURRENT_CYCLE_PERIOD
+        )
 
     def load_tag_config(self):
         try:
@@ -129,7 +252,9 @@ class TagBasedAccessPolicy:
         }
 
     async def _get_current_cycle(self, facility):
-        cycle_response = self.client.get(f"/v1/facility/{facility}/cycles/current")
+        cycle_response = await self.client.get(
+            f"/v1/facility/{facility}/cycles/current"
+        )
         cycle_response.raise_for_status()
         cycle = cycle_response.json()["cycle"]
         return cycle
@@ -137,11 +262,11 @@ class TagBasedAccessPolicy:
     async def _load_facility_api(self, facility, cycles=[]):
         if facility not in self.all_facilities:
             raise ValueError(f"Invalid {facility=} is not a known facility.")
-        valid_cycles_response = self.client.get(f"/v1/facility/{facility}/cycles")
+        valid_cycles_response = await self.client.get(f"/v1/facility/{facility}/cycles")
         valid_cycles_response.raise_for_status()
         valid_cycles = valid_cycles_response.json()["cycles"]
         if not cycles:
-            cycles_response = self.client.get(f"/v1/facility/{facility}/cycles")
+            cycles_response = await self.client.get(f"/v1/facility/{facility}/cycles")
             cycles_response.raise_for_status()
             cycles = cycles_response.json()["cycles"]
         elif not all(cycle in valid_cycles for cycle in cycles):
@@ -156,7 +281,7 @@ class TagBasedAccessPolicy:
                 page, page_size = 1, 100
                 count = page_size
                 while count == page_size:
-                    proposals_response = self.client.get(
+                    proposals_response = await self.client.get(
                         f"/v1/proposals/?beamline={beamline.upper()}&cycle={cycle}&facility={facility}&page_size={page_size}&page={page}&include_directories=false"
                     )
                     proposals_response.raise_for_status()
@@ -166,9 +291,9 @@ class TagBasedAccessPolicy:
                             intern(proposal["data_session"])
                         )
                     count = proposals_response_json["count"]
-                    page = page + 1
+                    page += 1
 
-            proposals_response = self.client.get(
+            proposals_response = await self.client.get(
                 f"/v1/proposals/commissioning?beamline={beamline}&facility={facility}"
             )
             proposals_response.raise_for_status()
@@ -245,10 +370,11 @@ class TagBasedAccessPolicy:
         Fetch any newly added proposals and load their tags,
         without changing the already-compiled tags
         """
-        await self.load_proposals_all_cycles()
-        self.create_tags_root_node()
-        self.compile()
-        self.load_compiled_tags()
+        async with TAG_SYNC_LOCK:
+            await self.load_proposals_all_cycles()
+            self.create_tags_root_node()
+            self.compile()
+            self.load_compiled_tags()
 
     async def reload_tags_all_cycles(self, clear_grp_cache=False):
         """
@@ -256,23 +382,37 @@ class TagBasedAccessPolicy:
         Optionally, clear the group_record_cache to also force group membership
         to be refreshed.
         """
-        self.load_tag_config()
-        await self.load_proposals_all_cycles()
-        self.create_tags_root_node()
-        self.recompile()
-        self.load_compiled_tags()
+        async with TAG_SYNC_LOCK:
+            logger.debug("Updating ALL cycles")
+            if clear_grp_cache:
+                group_record_cache.clear()
+            self.load_tag_config()
+            await self.load_proposals_all_cycles()
+            self.create_tags_root_node()
+            self.recompile()
+            self.load_compiled_tags()
 
     async def update_tags_current_cycle(self):
         """
         Fetch any newly added proposals and load their tags,
         without changing the already-compiled tags
         """
-        if clear_grp_cache:
-            group_record_cache.clear()
-        await self.load_proposals_current_cycle()
-        self.create_tags_root_node()
-        self.compile()
-        self.load_compiled_tags()
+        try:
+            logger.debug("Acquiring lock for sync of current cycle")
+            await asyncio.wait_for(TAG_SYNC_LOCK.acquire(), timeout=0.1)
+        except (TimeoutError, asyncio.TimeoutError):
+            logger.debug("Lock failed: skipping sync of current cycle")
+            return
+
+        try:
+            logger.debug("Syncing current cycle")
+            await self.load_proposals_current_cycle()
+            self.create_tags_root_node()
+            self.compile()
+            self.load_compiled_tags()
+        finally:
+            TAG_SYNC_LOCK.release()
+            logger.debug("Releasing lock...")
 
     async def reload_tags_current_cycle(self, clear_grp_cache=False):
         """
@@ -280,13 +420,25 @@ class TagBasedAccessPolicy:
         Optionally, clear the group_record_cache to also force group membership
         to be refreshed.
         """
-        if clear_grp_cache:
-            group_record_cache.clear()
-        self.load_tag_config()
-        await self.load_proposals_current_cycle()
-        self.create_tags_root_node()
-        self.recompile()
-        self.load_compiled_tags()
+        try:
+            logger.debug("Acquiring lock for sync of current cycle")
+            await asyncio.wait_for(TAG_SYNC_LOCK.acquire(), timeout=0.1)
+        except (TimeoutError, asyncio.TimeoutError):
+            logger.debug("Lock failed: skipping sync of current cycle")
+            return
+
+        try:
+            logger.debug("Syncing current cycle")
+            if clear_grp_cache:
+                group_record_cache.clear()
+            self.load_tag_config()
+            await self.load_proposals_current_cycle()
+            self.create_tags_root_node()
+            self.recompile()
+            self.load_compiled_tags()
+        finally:
+            TAG_SYNC_LOCK.release()
+            logger.debug("Releasing lock...")
 
     def create_tags_root_node(self):
         for facility in self.all_facilities:
@@ -299,30 +451,43 @@ class TagBasedAccessPolicy:
                 beamline_tag = intern(beamline_tag)
                 beamline_root_tag = intern(beamline_root_tag)
                 if beamline_tag in self.tags:
-                    self.tags[beamline_root_tag]["auto_tags"] = [
-                        {"name": beamline_tag},
-                        {"name": self.public_tag},
-                    ]
+                    self.tags[beamline_root_tag] = {
+                        "auto_tags": [
+                            {"name": beamline_tag},
+                            {"name": self.public_tag},
+                        ]
+                    }
 
     def _dfs(self, current_tag, tags, seen_tags, nested_level=0):
         if current_tag in self.compiled_tags:
-            return self.compiled_tags[current_tag]
+            return self.compiled_tags[current_tag], current_tag in self.compiled_public
         if current_tag in seen_tags:
-            return {}
+            return {}, False
         if nested_level > self.max_tag_nesting:
             raise RecursionError(
                 f"Exceeded maximum tag nesting of {max_nesting} levels"
             )
 
+        public_auto_tag = False
         seen_tags.add(current_tag)
         users = {}
         for tag in tags[current_tag]:
+            if tag.casefold() == self.public_tag:
+                public_auto_tag = True
+                continue
             try:
-                users.update(self._dfs(tag, tags, seen_tags, nested_level + 1))
+                child_users, child_public = self._dfs(
+                    tag, tags, seen_tags, nested_level + 1
+                )
+                public_auto_tag = public_auto_tag or child_public
+                users.update(child_users)
             except (RecursionError, ValueError) as e:
                 raise RuntimeError(
                     f"Tag compilation failed at tag: {current_tag}"
                 ) from e
+
+        if public_auto_tag:
+            self.compiled_public.add(current_tag)
 
         if "users" in self.tags[current_tag]:
             for user in self.tags[current_tag]["users"]:
@@ -391,7 +556,7 @@ class TagBasedAccessPolicy:
                         users[username].update(group_scopes)
 
         self.compiled_tags[current_tag] = users
-        return users
+        return users, public_auto_tag
 
     def compile(self):
         for role in self.roles.values():
@@ -407,10 +572,17 @@ class TagBasedAccessPolicy:
 
         adjacent_tags = {}
         for tag, members in self.tags.items():
+            if tag.casefold() == self.public_tag:
+                raise ValueError(
+                    f"'Public' tag '{self.public_tag}' cannot be redefined."
+                )
             adjacent_tags[tag] = set()
             if "auto_tags" in members:
                 for auto_tag in members["auto_tags"]:
-                    if auto_tag["name"] not in self.tags:
+                    if (
+                        auto_tag["name"] not in self.tags
+                        and auto_tag["name"].casefold() != self.public_tag
+                    ):
                         raise KeyError(
                             f"Tag '{tag}' has nested tag '{auto_tag}' which does not have a definition."
                         )
@@ -455,12 +627,14 @@ class TagBasedAccessPolicy:
 
     def recompile(self):
         self.compiled_tags = {}
+        self.compiled_public = set({self.public_tag})
         self.compiled_scopes = {}
         self.compiled_tag_owners = {}
         self.compile()
 
     def load_compiled_tags(self):
         self.loaded_tags = self.compiled_tags.copy()
+        self.loaded_public = self.compiled_public.copy()
         self.loaded_scopes = self.compiled_scopes.copy()
         self.loaded_tag_owners = self.compiled_tag_owners.copy()
 
@@ -621,9 +795,10 @@ class TagBasedAccessPolicy:
                     allowed = self.scopes
             elif "tags" in node.access_blob:
                 for tag in node.access_blob["tags"]:
-                    if tag == self.public_tag:
+                    if tag in self.loaded_public:
                         allowed.update(self.read_scopes)
-                        continue
+                        if tag == self.public_tag:
+                            continue
                     elif tag not in self.loaded_tags:
                         continue
                     tag_scopes = self.loaded_tags[tag].get(identifier, set())
@@ -654,7 +829,7 @@ class TagBasedAccessPolicy:
         tag_list = set.intersection(
             *[self.loaded_scopes[scope].get(identifier, set()) for scope in scopes],
             *[
-                self.public_tag if scope in self.read_scopes else set()
+                self.loaded_public if scope in self.read_scopes else set()
                 for scope in scopes
             ],
         )
