8bf416d420366dd8aace43969619fb3fc5bc3307
Dan Allan <dallan@bnl.gov>
2025-05-06 13:57:44 -0400

    Test nested paths.

--- tiled/_tests/test_mount_node.py
+++ tiled/_tests/test_mount_node.py
@@ -34,7 +34,6 @@ def test_mount_node(tmpdir):
                 "tree": "catalog",
                 "args": {
                     "uri": catalog_uri,
-                    "init_if_not_exists": True,
                     "writable_storage": [tmpdir / "data"],
                     "mount_node": "/A",
                 },
@@ -44,7 +43,6 @@ def test_mount_node(tmpdir):
                 "tree": "catalog",
                 "args": {
                     "uri": catalog_uri,
-                    "init_if_not_exists": True,
                     "writable_storage": [tmpdir / "data"],
                     "mount_node": "/B",
                 },
@@ -55,8 +53,46 @@ def test_mount_node(tmpdir):
         client = from_context(context)
         # Create a new node in each section of the catalog.
         client["a"].create_container("x")
+        client["a"]["x"].create_container("i")
         client["b"].create_container("y")
         # Check for no cross-talk.
         assert list(client) == ["a", "b"]
         assert list(client["a"]) == ["x"]
+        assert list(client["a"]["x"]) == ["i"]
         assert list(client["b"]) == ["y"]
+
+    # Mount a more deeply nested node at a nested path.
+    multi_tree_config = {
+        "trees": [
+            {
+                "path": "/some/nested/path",
+                "tree": "catalog",
+                "args": {
+                    "uri": catalog_uri,
+                    "writable_storage": [tmpdir / "data"],
+                    "mount_node": "/A/x",
+                },
+            },
+        ]
+    }
+    with Context.from_app(build_app_from_config(multi_tree_config)) as context:
+        client = from_context(context)
+        assert list(client["some"]["nested"]["path"]) == ["i"]
+
+    # As above, but specify the mount_node as a list of path segments.
+    multi_tree_config = {
+        "trees": [
+            {
+                "path": "/some/nested/path",
+                "tree": "catalog",
+                "args": {
+                    "uri": catalog_uri,
+                    "writable_storage": [tmpdir / "data"],
+                    "mount_node": ["A", "x"],
+                },
+            },
+        ]
+    }
+    with Context.from_app(build_app_from_config(multi_tree_config)) as context:
+        client = from_context(context)
+        assert list(client["some"]["nested"]["path"]) == ["i"]
