63674c976ef25f9fbbd223373637eaa54a167d9d
Dan Allan <dallan@bnl.gov>
2025-05-07 09:13:49 -0400

    Type hint; move validation; refactor logic for safety.

--- tiled/catalog/adapter.py
+++ tiled/catalog/adapter.py
@@ -12,7 +12,7 @@ import sys
 import uuid
 from functools import partial, reduce
 from pathlib import Path
-from typing import Callable, Dict
+from typing import Callable, Dict, List, Optional, Union
 from urllib.parse import urlparse
 
 import anyio
@@ -307,21 +307,24 @@ class CatalogNodeAdapter:
         queries=None,
         sorting=None,
         access_policy=None,
-        mount_node=None,
+        mount_node: Optional[Union[str, List[str]]] = None,
     ):
         self.context = context
         self.engine = self.context.engine
+        if isinstance(mount_node, str):
+            mount_node = [segment for segment in mount_node.split("/") if segment]
+        if mount_node:
+            if not isinstance(node, RootNode):
+                # sanity-check -- this should not be reachable
+                raise RuntimeError("mount_node should only be passed with the RootNode")
+            node.ancestors.extend(mount_node[:-1])
+            node.key = mount_node[-1]
         self.node = node
         if node.key is None:
             # Special case for RootNode
             self.segments = []
         else:
             self.segments = node.ancestors + [node.key]
-        if mount_node:
-            if not (isinstance(node, RootNode) and len(self.segments) == 0):
-                # sanity-check -- this should not be reachable
-                raise RuntimeError("mount_node should only be passed with the RootNode")
-            self.segments.extend(mount_node)
         self.sorting = sorting or [("", 1)]
         self.order_by_clauses = order_by_clauses(self.sorting)
         self.conditions = conditions or []
@@ -1489,7 +1492,7 @@ def from_uri(
     init_if_not_exists=False,
     echo=DEFAULT_ECHO,
     adapters_by_mimetype=None,
-    mount_node=None,
+    mount_node: Optional[Union[str, List[str]]] = None,
 ):
     uri = ensure_specified_sql_driver(uri)
     if init_if_not_exists:
@@ -1529,8 +1532,6 @@ def from_uri(
     )
     if engine.dialect.name == "sqlite":
         event.listens_for(engine.sync_engine, "connect")(_set_sqlite_pragma)
-    if isinstance(mount_node, str):
-        mount_node = [segment for segment in mount_node.split("/") if segment]
     adapter = CatalogContainerAdapter(
         Context(engine, writable_storage, readable_storage, adapters_by_mimetype),
         RootNode(metadata, specs, access_policy),
