382ce4f9ca6b201af168731b17aac6b321bf1e4c
Dan Allan <dallan@bnl.gov>
2025-05-07 10:07:33 -0400

    Mount nodes hotfix (#971)
    * REF: Reuse existing fixture.
    
    * Test mount_node against PG as well.
    
    * Fix mount_nodes on PG
    
    * Update CHANGELOG
    
    * Type hint; move validation; refactor logic for safety.

--- tiled/catalog/adapter.py
+++ tiled/catalog/adapter.py
@@ -1,4 +1,3 @@
-import asyncio
 import collections
 import copy
 import dataclasses
@@ -13,7 +12,7 @@ import sys
 import uuid
 from functools import partial, reduce
 from pathlib import Path
-from typing import Callable, Dict
+from typing import Callable, Dict, List, Optional, Union
 from urllib.parse import urlparse
 
 import anyio
@@ -308,9 +307,18 @@ class CatalogNodeAdapter:
         queries=None,
         sorting=None,
         access_policy=None,
+        mount_node: Optional[Union[str, List[str]]] = None,
     ):
         self.context = context
         self.engine = self.context.engine
+        if isinstance(mount_node, str):
+            mount_node = [segment for segment in mount_node.split("/") if segment]
+        if mount_node:
+            if not isinstance(node, RootNode):
+                # sanity-check -- this should not be reachable
+                raise RuntimeError("mount_node should only be passed with the RootNode")
+            node.ancestors.extend(mount_node[:-1])
+            node.key = mount_node[-1]
         self.node = node
         if node.key is None:
             # Special case for RootNode
@@ -1484,7 +1492,7 @@ def from_uri(
     init_if_not_exists=False,
     echo=DEFAULT_ECHO,
     adapters_by_mimetype=None,
-    mount_node=None,
+    mount_node: Optional[Union[str, List[str]]] = None,
 ):
     uri = ensure_specified_sql_driver(uri)
     if init_if_not_exists:
@@ -1528,15 +1536,8 @@ def from_uri(
         Context(engine, writable_storage, readable_storage, adapters_by_mimetype),
         RootNode(metadata, specs, access_policy),
         access_policy=access_policy,
+        mount_node=mount_node,
     )
-    if isinstance(mount_node, str):
-        mount_node = [segment for segment in mount_node.split("/") if segment]
-    if mount_node:
-
-        async def get_nested_node():
-            return await adapter.lookup_adapter(mount_node)
-
-        adapter = asyncio.run(get_nested_node())
     return adapter
 
 
