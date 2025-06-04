77407912f47888f7cd551772fcceb469d24e6be9
jessica-moylan <jessicamoylan10@gmail.com>
2025-06-04 09:00:39 -0400

    changed format

--- tiled/server/simple.py
+++ tiled/server/simple.py
@@ -123,7 +123,7 @@ class SimpleTiledServer:
             directory / "catalog.db",
             writable_storage=directory / "data",
             init_if_not_exists=True,
-            readable_storage= directory / "data"
+            readable_storage= [str(directory / "data")]
         )
         self.app = build_app(
             self.catalog, authentication={"single_user_api_key": api_key}
