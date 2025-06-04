cfc710d3aa0dde83dda34fba21149b66a3a9f684
jessica-moylan <jessicamoylan10@gmail.com>
2025-06-04 08:41:02 -0400

    generalized readable_storage

--- tiled/server/simple.py
+++ tiled/server/simple.py
@@ -123,7 +123,7 @@ class SimpleTiledServer:
             directory / "catalog.db",
             writable_storage=directory / "data",
             init_if_not_exists=True,
-            readable_storage= ["/tmp/blop/sim"]
+            readable_storage= directory
         )
         self.app = build_app(
             self.catalog, authentication={"single_user_api_key": api_key}
