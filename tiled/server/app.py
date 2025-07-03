6f1111db01f580c801c8aa1902a9ad27e72336b1
Shekar V <vshekar1@bnl.gov>
2025-07-03 15:55:58 -0400

    Connect with websockets

--- tiled/server/app.py
+++ tiled/server/app.py
@@ -216,7 +216,7 @@ or via the environment variable TILED_SINGLE_USER_API_KEY.""",
         yield
         await shutdown_event()
 
-    app = FastAPI(lifespan=lifespan, dependencies=[Depends(move_api_key)])
+    app = FastAPI(lifespan=lifespan) #, dependencies=[Depends(move_api_key)])
 
     # Healthcheck for deployment to containerized systems, needs to preempt other responses.
     # Standardized for Kubernetes, but also used by other systems.
