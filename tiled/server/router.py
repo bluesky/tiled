76797840518b59a2d0b934c51c5c92a2aef439bc
Shekar V <vshekar1@bnl.gov>
2025-07-03 14:58:33 -0400

    Initial websocket endpoint implementation

--- tiled/server/router.py
+++ tiled/server/router.py
@@ -53,6 +53,7 @@ from . import schemas
 from .authentication import (
     check_scopes,
     get_current_principal,
+    get_current_principal_websocket,
     get_current_scopes,
     get_session_state,
 )
@@ -636,12 +637,23 @@ def get_router(
                                  path: str, 
                                  envelope_format: str = "json", 
                                  seq_num: Optional[int] = None,
-                                 principal: Union[Principal, SpecialUsers] = Depends(get_current_principal),
-                                 root_tree: pydantic_settings.BaseSettings = Depends(get_root_tree),
-                                 session_state: dict = Depends(get_session_state),
-                                 _=Security(check_scopes),
+                                 # principal: Union[Principal, SpecialUsers] = Depends(get_current_principal_websocket),
+                                 # root_tree: pydantic_settings.BaseSettings = Depends(get_root_tree),
+                                 # session_state: dict = Depends(get_session_state),
+                                 # authn_scopes: Scopes = Depends(get_current_scopes),
+                                 # _=Security(check_scopes, scopes=["read:data", "read:metadata"]),
                                 ):
-        entry = await get_entry(path, ["read:data", "read:metadata"], principal, root_tree, session_state, None)
+        # entry = await get_entry(path, 
+        #                         ["read:data", "read:metadata"],
+        #                         principal,
+        #                         set(["read:data"]),# authn_scopes,
+        #                         root_tree,
+        #                         None, #session_state,
+        #                         {StructureFamily.array, StructureFamily.sparse},
+        #                         websocket.state.metrics,
+        #                         getattr(websocket.app.state, "access_policy", None),
+        #                         )
+        entry = None
         import asyncio
         import json
         import msgpack
