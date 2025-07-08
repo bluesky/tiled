7f83bd507f87eb6c3af1ef4ecac4e6044ed390c7
Dan Allan <dallan@bnl.gov>
2025-07-08 18:30:29 -0400

    redis must be optional

--- tiled/catalog/adapter.py
+++ tiled/catalog/adapter.py
@@ -1606,6 +1606,7 @@ def from_uri(
         redis_ttl = redis_settings.get("ttl", 3600)
     else:
         redis_client = None
+        redis_ttl = 0
     
     context = Context(
         engine, 
