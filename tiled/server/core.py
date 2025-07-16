6e982247bbd55f85018b07a8268e0984445cc061
Shekar V <vshekar1@bnl.gov>
2025-07-16 12:46:10 -0400

    Renamed encoder to envelope_formatter for consistency

--- tiled/server/core.py
+++ tiled/server/core.py
@@ -744,7 +744,7 @@ def json_or_msgpack(
     )
 
 
-def get_websocket_encoder(
+def get_websocket_envelope_formatter(
     envelope_format: schemas.EnvelopeFormat, entry, deserialization_registry
 ):
     if envelope_format == "msgpack":
