967f8dfe571b1956456bb745f8be31f84188c37b
nmaytan <nmaytan@bnl.gov>
2025-07-22 20:36:00 -0400

    Allow in-memory catalogs to be shared in-process

--- tiled/catalog/adapter.py
+++ tiled/catalog/adapter.py
@@ -271,6 +271,7 @@ class CatalogNodeAdapter:
     async def startup(self):
         if (self.context.engine.dialect.name == "sqlite") and (
             self.context.engine.url.database == ":memory:"
+            or self.context.engine.url.query.get("mode", None) == "memory"
         ):
             # Special-case for in-memory SQLite: Because it is transient we can
             # skip over anything related to migrations.
@@ -1473,6 +1474,7 @@ CatalogNodeAdapter.register_query(Like, like)
 
 def in_memory(
     *,
+    named_memory=None,
     metadata=None,
     specs=None,
     writable_storage=None,
@@ -1481,7 +1483,10 @@ def in_memory(
     adapters_by_mimetype=None,
     top_level_access_blob=None,
 ):
-    uri = "sqlite:///:memory:"
+    if not named_memory:
+        uri = "sqlite:///:memory:"
+    else:
+        uri = f"sqlite:///file:{named_memory}?mode=memory&cache=shared&uri=true"
     return from_uri(
         uri=uri,
         metadata=metadata,
@@ -1529,8 +1534,10 @@ def from_uri(
         logger.info(f"Subprocess stderr: {stderr}")
 
     parsed_url = make_url(uri)
-    if (parsed_url.get_dialect().name == "sqlite") and (
-        parsed_url.database != ":memory:"
+    if (
+        (parsed_url.get_dialect().name == "sqlite")
+        and (parsed_url.database != ":memory:")
+        and (parsed_url.query.get("mode", None) != "memory")
     ):
         # For file-backed SQLite databases, connection pooling offers a
         # significant performance boost. For SQLite databases that exist
