8aac0a7495caee13c9eefaf06ac95080344030aa
Eugene M <ymatviych@bnl.gov>
2025-08-01 11:30:31 -0400

    ENH: add node_id index in data_sources table

--- tiled/catalog/core.py
+++ tiled/catalog/core.py
@@ -6,6 +6,7 @@ from .base import Base
 
 # This is list of all valid revisions (from current to oldest).
 ALL_REVISIONS = [
+    "7809873ea2c7",
     "9331ed94d6ac",
     "45a702586b2a",
     "ed3a4223a600",
