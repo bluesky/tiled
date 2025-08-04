ed1024135e9448a77806eb5781366267e7775f0b
gbischof <bisc8233@gmail.com>
2025-08-04 17:05:02 -0400

    add test_locust.py because I am getting auth errors

---
+++ locust/test_locust.py
@@ -0,0 +1,20 @@
+
+from locust import HttpUser, task, between
+import os
+
+class TiledUser(HttpUser):
+    wait_time = between(1, 3)
+
+    def on_start(self):
+        self.api_key = os.environ.get('TILED_DEV_API_KEY', 'secret')
+        self.headers = {'Authorization': f'Bearer {self.api_key}'}
+
+    @task
+    def get_root(self):
+        """Test getting the root endpoint"""
+        self.client.get("/", headers=self.headers)
+
+    @task
+    def get_metadata(self):
+        """Test getting metadata endpoint"""
+        self.client.get("/api/v1/metadata", headers=self.headers)
