03115cb1bf60685e749beb6207d20899f37441c5
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-08-19 13:21:49 +0000

    Remove AnyStructure

--- tiled/type_aliases.py
+++ tiled/type_aliases.py
@@ -1,16 +1,11 @@
 import sys
 
-from tiled.structures.array import ArrayStructure
-from tiled.structures.awkward import AwkwardStructure
-from tiled.structures.sparse import SparseStructure
-from tiled.structures.table import TableStructure
-
 if sys.version_info < (3, 10):
     EllipsisType = type(Ellipsis)
 else:
     from types import EllipsisType
 
-from typing import Any, List, Mapping, Sequence, Set, Union
+from typing import Any, List, Mapping, Sequence, Set
 
 JSON_ITEM = str | int | float | Mapping[str, "JSON_ITEM"] | Sequence["JSON_ITEM"] | None
 JSON = Mapping[str, JSON_ITEM]
@@ -19,10 +14,7 @@ Scopes = Set[str]
 Query = Any  # for now...
 Filters = List[Query]
 
-AnyStructure = Union[TableStructure, ArrayStructure, SparseStructure, AwkwardStructure]
-
 __all__ = [
-    "AnyStructure",
     "EllipsisType",
     "JSON",
     "Scopes",
