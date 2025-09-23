9f12ed1ab884b2394e6edc7c357239638fc6901d
kari Barry <kezzsim@gmail.com>
2025-09-23 17:51:58 -0400

    😢 attempt to put my changes back (which breaks everything)

--- tiled/catalog/adapter.py
+++ tiled/catalog/adapter.py
@@ -179,8 +179,8 @@ class Context:
     ):
         self.engine = engine
 
-        self.writable_storage = []
-        self.readable_storage = set()
+        self.writable_storage = {}
+        self.readable_storage = {}
 
         # Back-compat: `writable_storage` used to be a dict: we want its values.
         if isinstance(writable_storage, dict):
@@ -192,18 +192,16 @@ class Context:
             raise ValueError("readable_storage should be a list of URIs or paths")
 
         for item in writable_storage or []:
-            self.writable_storage.append(
-                parse_storage(
-                    item, pool_size=storage_pool_size, max_overflow=storage_max_overflow
-                )
-            )
+            storage = parse_storage(item)
+            self.writable_storage[storage.uri] = storage
         for item in readable_storage or []:
-            self.readable_storage.add(parse_storage(item))
+            storage = parse_storage(item)
+            self.readable_storage[storage.uri] = storage
         # Writable storage should also be readable.
         self.readable_storage.update(self.writable_storage)
         # Register all storage in a registry that enables Adapters to access
         # credentials (if applicable).
-        for item in self.readable_storage:
+        for item in self.readable_storage.values():
             register_storage(item)
 
         self.key_maker = key_maker
@@ -313,7 +311,14 @@ class CatalogNodeAdapter:
         return self.node.metadata_
 
     async def startup(self):
-        await self.context.startup()
+        if (self.context.engine.dialect.name == "sqlite") and (
+            self.context.engine.url.database == ":memory:"
+        ):
+            # Special-case for in-memory SQLite: Because it is transient we can
+            # skip over anything related to migrations.
+            await initialize_database(self.context.engine)
+        else:
+            await check_catalog_database(self.context.engine)
 
     async def create_mount(self, mount_path: list[str]):
         statement = node_from_segments(mount_path).with_only_columns(orm.Node.id)
@@ -528,7 +533,7 @@ class CatalogNodeAdapter:
                 asset_path = path_from_uri(asset.data_uri)
                 for readable_storage in {
                     item
-                    for item in self.context.readable_storage
+                    for item in self.context.readable_storage.values()
                     if isinstance(item, FileStorage)
                 }:
                     if (
@@ -718,7 +723,7 @@ class CatalogNodeAdapter:
                     supported_storage = getattr(
                         adapter_cls, "supported_storage", {FileStorage}
                     )
-                    for storage in self.context.writable_storage:
+                    for storage in self.context.writable_storage.values():
                         if isinstance(storage, tuple(supported_storage)):
                             break
                     else:
