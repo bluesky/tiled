1f31636ad51eb4fbeb94223d05d13cf4bbd5f20b
Eugene M <ymatviych@bnl.gov>
2025-10-20 11:44:51 -0400

    FIX: allow extra kwargs in HDF5Adapter from_uris

--- tiled/adapters/hdf5.py
+++ tiled/adapters/hdf5.py
@@ -446,6 +446,7 @@ class HDF5Adapter(
         swmr: bool = SWMR_DEFAULT,
         libver: str = "latest",
         locking: Optional[Union[bool, str]] = None,
+        **kwargs: Any,  # Optional kwargs for HDF5ArrayAdapter
     ) -> Union["HDF5Adapter", HDF5ArrayAdapter]:
         fpath = path_from_uri(data_uris[0])
         with h5open(fpath, dataset, swmr=swmr, libver=libver, locking=locking) as file:
@@ -453,7 +454,12 @@ class HDF5Adapter(
 
         if tree == HDF5_DATASET:
             return HDF5ArrayAdapter.from_uris(
-                *data_uris, dataset=dataset, swmr=swmr, libver=libver, locking=locking
+                *data_uris,
+                dataset=dataset,
+                swmr=swmr,
+                libver=libver,
+                locking=locking,
+                **kwargs,
             )
 
         return cls(
