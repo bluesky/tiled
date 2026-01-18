3b33dfed735c0c8418035d3ab341e2e1edc67255
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2026-01-18 17:59:26 +0000

    WIP To start all services using docker compose

--- continuous_integration/scripts/download_sqlite_data.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -e
-
-curl -fsSLO https://github.com/bluesky/tiled-example-database/releases/latest/download/tiled_test_db_sqlite.db
--- continuous_integration/scripts/start_redis.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-docker run -d --rm --name tiled-test-redis -p 6379:6379 docker.io/redis:7-alpine
-docker ps
