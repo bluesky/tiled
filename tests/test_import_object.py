3468c75b8a1e6cda932106c52e947febb9cd2f4b
Dan Allan <dallan@bnl.gov>
2026-01-22 16:55:07 -0500

    Update relative path to example_configs

--- tests/test_import_object.py
+++ tests/test_import_object.py
@@ -12,7 +12,7 @@ here = Path(__file__).parent.absolute()
 
 def test_config_imports_custom_python_module():
     "Configs can import from Python modules located in their same directory."
-    config_path = here / ".." / ".." / "example_configs" / "custom_export_formats"
+    config_path = here / ".." / "example_configs" / "custom_export_formats"
     parsed_config = parse_configs(config_path)
     build_app_from_config(parsed_config)
 
