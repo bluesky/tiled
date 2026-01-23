259a986cfd76c1adecb4a6c86c2e0718007b7ad2
Connor Boyle <connorboyle@sasktel.net>
2026-01-22 22:21:05 -0600

    speed up tests, no need to restart for different slices

--- tiled/_tests/test_ragged.py
+++ tiled/_tests/test_ragged.py
@@ -8,13 +8,13 @@ import ragged
 from tiled.catalog import in_memory
 from tiled.client import Context, from_context, record_history
 from tiled.serialization.ragged import (
+    from_json,
     from_numpy_array,
     from_numpy_octet_stream,
-    from_json,
     from_zipped_buffers,
+    to_json,
     to_numpy_array,
     to_numpy_octet_stream,
-    to_json,
     to_zipped_buffers,
 )
 from tiled.server.app import build_app
@@ -62,7 +62,12 @@ arrays = {
     "ragged_a": ragged.array([RNG.random(3), RNG.random(5), RNG.random(8)]),
     "ragged_b": ragged.array([RNG.random((2, 3, 4)), RNG.random((3, 4, 5))]),
     "ragged_c": ragged.array(
-        [[RNG.random(10)], [RNG.random(8), []], [RNG.random(5), RNG.random(2)], [[], RNG.random(7)]]
+        [
+            [RNG.random(10)],
+            [RNG.random(8), []],
+            [RNG.random(5), RNG.random(2)],
+            [[], RNG.random(7)],
+        ]
     ),
     "ragged_d": ragged.array(
         [
@@ -97,36 +102,48 @@ def test_serialization_roundtrip(name):
 
     # Test JSON serialization.
     json_contents = to_json("application/json", array, metadata={})
-    array_from_json = from_json(json_contents, dtype=array.dtype.type, offsets=structure.offsets, shape=structure.shape)
+    array_from_json = from_json(
+        json_contents,
+        dtype=array.dtype.type,
+        offsets=structure.offsets,
+        shape=structure.shape,
+    )
     assert ak.array_equal(array._impl, array_from_json._impl)  # noqa: SLF001
 
     # Test reduced/flattened numpy array.
     reduced_array = to_numpy_array(array)
     array_from_flattened = from_numpy_array(
-        reduced_array, dtype=array.dtype.type, offsets=structure.offsets, shape=structure.shape
+        reduced_array,
+        dtype=array.dtype.type,
+        offsets=structure.offsets,
+        shape=structure.shape,
     )
     assert ak.array_equal(array._impl, array_from_flattened._impl)  # noqa: SLF001
 
     # Test flattened octet-stream serialization.
-    octet_stream_contents = to_numpy_octet_stream("application/octet-stream", array, metadata={})
+    octet_stream_contents = to_numpy_octet_stream(
+        "application/octet-stream", array, metadata={}
+    )
     array_from_octet_stream = from_numpy_octet_stream(
-        octet_stream_contents, dtype=array.dtype.type, offsets=structure.offsets, shape=structure.shape
+        octet_stream_contents,
+        dtype=array.dtype.type,
+        offsets=structure.offsets,
+        shape=structure.shape,
     )
     assert ak.array_equal(array._impl, array_from_octet_stream._impl)  # noqa: SLF001
 
     # Test flattened octet-stream serialization.
     octet_stream_contents = to_zipped_buffers("application/zip", array, metadata={})
-    array_from_octet_stream = from_zipped_buffers(octet_stream_contents, dtype=array.dtype.type)
+    array_from_octet_stream = from_zipped_buffers(
+        octet_stream_contents, dtype=array.dtype.type
+    )
     assert ak.array_equal(array._impl, array_from_octet_stream._impl)  # noqa: SLF001
 
 
 @pytest.mark.parametrize("name", arrays.keys())
-@pytest.mark.parametrize("slice", [(1,), (1, 0), (slice(0, -1), 0)])
-def test_slicing(client, name, slice):
+def test_slicing(client, name):
     # Write data into catalog.
     array = arrays[name]
-    if len(slice) > array.ndim:
-        pytest.skip("Slice has too many dimensions for array")
 
     returned = client.write_ragged(array, key="test")
     # Test with client returned, and with client from lookup.
@@ -142,9 +159,31 @@ def test_slicing(client, name, slice):
         assert ak.array_equal(full_result._impl, array._impl)  # noqa: SLF001
         assert len(h.responses) == 1  # sanity check
         full_response_size = len(h.responses[0].content)
+
+        # index at first dimension
         with record_history() as h:
-            sliced_result = rac[slice]
-        assert ak.array_equal(sliced_result._impl, array[slice]._impl)  # noqa: SLF001
+            sliced_result = rac[1]
+        assert ak.array_equal(sliced_result._impl, array[1]._impl)  # noqa: SLF001
+        assert len(h.responses) == 1  # sanity check
+        sliced_response_size = len(h.responses[0].content)
+        assert sliced_response_size < full_response_size
+
+        if len(array.shape) < 2:
+            # next slices will produce expected errors
+            continue
+
+        # index at first and second dimension
+        with record_history() as h:
+            sliced_result = rac[1, 0]
+        assert ak.array_equal(sliced_result._impl, array[1, 0]._impl)  # noqa: SLF001
+        assert len(h.responses) == 1  # sanity check
+        sliced_response_size = len(h.responses[0].content)
+        assert sliced_response_size < full_response_size
+
+        # index at second dimension
+        with record_history() as h:
+            sliced_result = rac[:, 0]
+        assert ak.array_equal(sliced_result._impl, array[:, 0]._impl)  # noqa: SLF001
         assert len(h.responses) == 1  # sanity check
         sliced_response_size = len(h.responses[0].content)
         assert sliced_response_size < full_response_size
