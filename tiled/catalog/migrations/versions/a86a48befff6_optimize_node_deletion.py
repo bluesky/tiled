67077da416676a2ad9c99fbfe23d0128ed32b441
Eugene M. <ymatviych@bnl.gov>
2026-01-23 14:34:46 -0500

    MNT: init alembic migration scripts

---
+++ tiled/catalog/migrations/versions/a86a48befff6_optimize_node_deletion.py
@@ -0,0 +1,24 @@
+"""Optimize node deletion
+
+Revision ID: a86a48befff6
+Revises: dfbb7478c6bd
+Create Date: 2026-01-23 14:31:23.869799
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a86a48befff6'
+down_revision = 'dfbb7478c6bd'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
