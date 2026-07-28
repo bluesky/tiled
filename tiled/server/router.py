efcdc6364cb8760deeaf136495bfb598147d6e83
Sam Welborn <swelborn@slac.stanford.edu>
2026-07-28 11:31:21 -0400

    BUG: include /api in the root_path reported by the About endpoint

--- tiled/server/router.py
+++ tiled/server/router.py
@@ -305,7 +305,10 @@ def get_router(
                     "self": base_url,
                     "documentation": f"{base_url}/docs",
                 },
-                meta={"root_path": request.scope.get("root_path") or "" + "/api"},
+                meta={
+                    "root_path": (request.scope.get("root_path") or "").rstrip("/")
+                    + "/api"
+                },
             ).model_dump(),
             expires=datetime.now(timezone.utc) + timedelta(seconds=600),
         )
