99eede6f24d5ce85fd7d06e1cdf9b7818e26fe2a
Dylan McReynolds <dmcreynolds@lbl.gov>
2026-07-29 13:02:17 -0700

    removed reference to graph database initialization

--- initdb/01-create-databases.sh
+++ initdb/01-create-databases.sh
@@ -3,5 +3,4 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
     CREATE DATABASE tiled_catalog;
     CREATE DATABASE tiled_storage;
-    CRAEATE DATABASE tiled_graph;
 EOSQL
