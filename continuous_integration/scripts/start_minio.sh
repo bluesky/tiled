e29efef66e779aa967ecf2c7b3c11eeec5ed5277
Zoheb Shaikh <26975142+ZohebShaikh@users.noreply.github.com>
2026-09-16 14:48:11 +0100

    ci: change minio container registry

--- continuous_integration/scripts/start_minio.sh
+++ continuous_integration/scripts/start_minio.sh
@@ -2,6 +2,6 @@
 set -e
 
 # Start MinIO server in docker container
-docker pull minio/minio:latest
+docker pull coollabsio/minio:latest
 docker compose -f continuous_integration/docker-configs/minio-docker-compose.yml up -d
 docker ps
